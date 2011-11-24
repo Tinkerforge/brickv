2b633d4de0ccda7e80b1d81d893094676f9900c3
Olaf Lüke <olaf@tinkerforge.com>
2011-11-24 10:02:13 +0100

    remove setting of position at start, we do this in servo firmware now

--- src/brickv/plugin_system/plugins/servo/servo.py
+++ src/brickv/plugin_system/plugins/servo/servo.py
@@ -249,10 +249,6 @@ class Servo(PluginBase, Ui_Servo):
             
         self.update_servo_specific()
         
-        for i in range(7):
-            if not self.servo.is_enabled(i):
-                self.servo.set_position(i, 0)
-            
     def destroy(self):
         self.test_event.set()
         self.update_event.set()
