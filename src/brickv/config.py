fe7da7a722268c62b768f6295150b3188e755ec9
Matthias Bolte <matthias@tinkerforge.com>
2012-06-04 17:53:48 +0200

    Update changelog

--- src/brickv/config.py
+++ src/brickv/config.py
@@ -8,4 +8,4 @@ LOGGING_LEVEL = logging.ERROR
 LOGGING_FORMAT = "%(asctime)s <%(levelname)s> <%(filename)s:%(lineno)s> %(message)s"
 LOGGING_DATEFMT = "%H:%M:%S"
 
-BRICKV_VERSION = "1.0.8"
+BRICKV_VERSION = "1.1.0"
