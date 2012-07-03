d3bf9da624e13093c66776cc087eff3fcfa685f1
Matthias Bolte <matthias@tinkerforge.com>
2012-07-03 09:51:47 +0200

    macos: Avoid space in .dmg volume name

--- src/brickv/_build_dmg.sh
+++ src/brickv/_build_dmg.sh
@@ -3,4 +3,4 @@ dot_version=`grep BRICKV_VERSION config.py | sed -e 's/BRICKV_VERSION = "\(.*\)"
 underscore_version=`printf %s $dot_version | sed -e 's/\./_/g'`
 dmg=brickv_macos_$underscore_version.dmg
 rm $dmg
-hdiutil create -fs HFS+ -volname "Brickv $dot_version" -srcfolder dist $dmg
+hdiutil create -fs HFS+ -volname "Brickv-$dot_version" -srcfolder dist $dmg
