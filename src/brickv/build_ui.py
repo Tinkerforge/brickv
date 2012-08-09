55eb9fd7b91c206441be48c7486b87beedad62e2
Tobias Dreyer <dreyer@molgen.mpg.de>
2012-08-09 12:28:37 +0200

    rename brickv.ui to mainwindow.ui to allow automated file generation via Makefile

--- src/brickv/build_ui.py
+++ src/brickv/build_ui.py
@@ -3,7 +3,7 @@
 import os
 
 # main window
-os.system("pyuic4 -o ui_mainwindow.py ui/brickv.ui")
+os.system("pyuic4 -o ui_mainwindow.py ui/mainwindow.ui")
 os.system("pyuic4 -o ui_flashing.py ui/flashing.ui")
 os.system("pyuic4 -o ui_advanced.py ui/advanced.ui")
 
