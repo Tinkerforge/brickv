2a22a442e7b39e56892fdec0c8a03586a28ddb7c
Matthias Bolte <matthias@tinkerforge.com>
2013-02-22 19:05:34 +0100

    analog in: Add support for measurement range selection

--- src/brickv/plugin_system/plugins/analog_in/analog_in.py
+++ src/brickv/plugin_system/plugins/analog_in/analog_in.py
@@ -27,7 +27,7 @@ from bindings import ip_connection
 from bindings.bricklet_analog_in import BrickletAnalogIn
 from async_call import async_call
 
-from PyQt4.QtGui import QVBoxLayout, QLabel, QHBoxLayout
+from PyQt4.QtGui import QVBoxLayout, QLabel, QHBoxLayout, QComboBox
 from PyQt4.QtCore import pyqtSignal, Qt
         
 class VoltageLabel(QLabel):
@@ -53,17 +53,39 @@ class AnalogIn(PluginBase):
         
         plot_list = [['', Qt.red, self.get_current_value]]
         self.plot_widget = PlotWidget('Voltage [mV]', plot_list)
-        
-        layout_h = QHBoxLayout()
-        layout_h.addStretch()
-        layout_h.addWidget(self.voltage_label)
-        layout_h.addStretch()
+
+        if self.version >= (2, 0, 1):
+            self.combo_range = QComboBox()
+            self.combo_range.addItem('Automatic')
+            self.combo_range.addItem('0V - 6.05V')
+            self.combo_range.addItem('0V - 10.32V')
+            self.combo_range.addItem('0V - 36.30V')
+            self.combo_range.addItem('0V - 45.00V')
+            self.combo_range.currentIndexChanged.connect(self.range_changed)
+
+            layout_h1 = QHBoxLayout()
+            layout_h1.addStretch()
+            layout_h1.addWidget(QLabel('Range:'))
+            layout_h1.addWidget(self.combo_range)
+            layout_h1.addStretch()
+
+        layout_h2 = QHBoxLayout()
+        layout_h2.addStretch()
+        layout_h2.addWidget(self.voltage_label)
+        layout_h2.addStretch()
 
         layout = QVBoxLayout(self)
-        layout.addLayout(layout_h)
+        if self.version >= (2, 0, 1):
+            layout.addLayout(layout_h1)
+        layout.addLayout(layout_h2)
         layout.addWidget(self.plot_widget)
-        
+
+    def get_range_async(self, range):
+        self.combo_range.setCurrentIndex(range)
+
     def start(self):
+        if self.version >= (2, 0, 1):
+            async_call(self.ai.get_range, None, self.get_range_async, self.increase_error_count)
         async_call(self.ai.get_voltage, None, self.cb_voltage, self.increase_error_count)
         async_call(self.ai.set_voltage_callback_period, 100, None, self.increase_error_count)
         
@@ -87,3 +109,7 @@ class AnalogIn(PluginBase):
     def cb_voltage(self, voltage):
         self.current_value = voltage
         self.voltage_label.setText(str(voltage/1000.0))
+
+    def range_changed(self, index):
+        if index >= 0 and self.version >= (2, 0, 1):
+            async_call(self.ai.set_range, index, None, self.increase_error_count)
