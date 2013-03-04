1f80aad63e19ccfef07f5300bd947800bb82653e
Matthias Bolte <matthias@tinkerforge.com>
2013-03-04 10:05:12 +0100

    Add missing QMessageBox import for Chibi plugin

--- src/brickv/plugin_system/plugins/master/chibi.py
+++ src/brickv/plugin_system/plugins/master/chibi.py
@@ -24,7 +24,7 @@ Boston, MA 02111-1307, USA.
 
 from bindings.ip_connection import IPConnection
 
-from PyQt4.QtGui import QWidget
+from PyQt4.QtGui import QWidget, QMessageBox
 
 from ui_chibi import Ui_Chibi
 
