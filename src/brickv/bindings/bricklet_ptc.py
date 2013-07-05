380066e94229dd9cee9288b6eb43df330ccff0b7
Matthias Bolte <matthias@tinkerforge.com>
2013-07-05 10:50:01 +0200

    Update bindings

--- src/brickv/bindings/bricklet_ptc.py
+++ src/brickv/bindings/bricklet_ptc.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
 #############################################################
-# This file was automatically generated on 2013-07-01.      #
+# This file was automatically generated on 2013-07-04.      #
 #                                                           #
-# Bindings Version 2.0.7                                    #
+# Bindings Version 2.0.8                                    #
 #                                                           #
 # If you have a bugfix for this file and want to commit it, #
 # please fix the bug in the generator. You can find a link  #
