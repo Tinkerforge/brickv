6e191c00770784da421b0ecdc4a96ee934c2178d
Olaf Lüke <olaf@tinkerforge.com>
2013-08-20 16:02:43 +0200

    Add first version of plugin for Rotary Encoder Bricklet

--- src/brickv/plugin_system/plugins/rotary_encoder/rotary_encoder.py
+++ src/brickv/plugin_system/plugins/rotary_encoder/rotary_encoder.py
@@ -25,26 +25,56 @@ from plugin_system.plugin_base import PluginBase
 from bindings.bricklet_rotary_encoder import BrickletRotaryEncoder
 from async_call import async_call
 
-from PyQt4.QtGui import QLabel, QVBoxLayout
+from PyQt4.QtGui import QLabel, QVBoxLayout, QHBoxLayout, QPushButton
+from PyQt4.QtCore import pyqtSignal, Qt
+    
+class CountLabel(QLabel):
+    def setText(self, text):
+        text = "Count: " + text
+        super(CountLabel, self).setText(text)
     
 class RotaryEncoder(PluginBase):
+    qtcb_count = pyqtSignal(int)
     
     def __init__(self, ipcon, uid, version):
         PluginBase.__init__(self, ipcon, uid, 'Rotary Encoder Bricklet', version)
 
         self.re = BrickletRotaryEncoder(uid, ipcon)
         
+        self.qtcb_count.connect(self.cb_count)
+        self.re.register_callback(self.re.CALLBACK_COUNT,
+                                  self.qtcb_count.emit) 
+        
+        self.count_label = CountLabel('Count: 0')
+        self.reset_button = QPushButton('Reset Count')
+        self.reset_button.pressed.connect(self.reset_pressed)
+        
+        
+        layout_h1 = QHBoxLayout()
+        layout_h1.addWidget(self.count_label)
+        layout_h1.addStretch()
+
+        layout_h2 = QHBoxLayout()
+        layout_h2.addWidget(self.reset_button)
+        layout_h2.addStretch()
+
         layout = QVBoxLayout(self)
-        layout.addStretch()
-        layout.addWidget(QLabel("The Bricklet is not yet supported in this version of Brickv. Please Update Brickv."))
+        layout.addLayout(layout_h1)
+        layout.addLayout(layout_h2)
         layout.addStretch()
         
+    def cb_count(self, count):
+        self.count_label.setText(str(count))
+        
+    def reset_pressed(self):
+        async_call(self.re.get_count, True, None, self.increase_error_count)
+        self.count_label.setText(str(0))
 
     def start(self):
-        pass
+        async_call(self.re.set_count_callback_period, 100, None, self.increase_error_count)
         
     def stop(self):
-        pass
+        async_call(self.re.set_count_callback_period, 0, None, self.increase_error_count)
 
     def get_url_part(self):
         return 'rotary_encoder'
