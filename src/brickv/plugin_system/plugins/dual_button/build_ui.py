a87a915d1781bb61b08e1ad7e3d236704a8a64da
Olaf Lüke <olaf@tinkerforge.com>
2013-08-20 15:28:39 +0200

    Add first version of Dual Button Bricklet plugin

---
+++ src/brickv/plugin_system/plugins/dual_button/build_ui.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python
+
+import os
+
+os.system("pyuic4 -o ui_dual_button.py ui/dual_button.ui")
--- src/brickv/plugin_system/plugins/dual_button/dual_button.py
+++ src/brickv/plugin_system/plugins/dual_button/dual_button.py
@@ -25,23 +25,137 @@ from plugin_system.plugin_base import PluginBase
 from bindings.bricklet_dual_button import BrickletDualButton
 from async_call import async_call
 
-from PyQt4.QtGui import QLabel, QVBoxLayout
+from ui_dual_button import Ui_DualButton
+from PyQt4.QtCore import pyqtSignal
     
-class DualButton(PluginBase):
+class DualButton(PluginBase, Ui_DualButton):
+    qtcb_state_changed = pyqtSignal(int, int, int, int)
+    
+    AT_ON = 0
+    AT_OFF = 1
+    ON = 2
+    OFF = 3
+    
+    PRESSED = 0
+    RELEASED = 1
     
     def __init__(self, ipcon, uid, version):
         PluginBase.__init__(self, ipcon, uid, 'Dual Button Bricklet', version)
+        
+        self.setupUi(self)
 
         self.button = BrickletDualButton(uid, ipcon)
         
-        layout = QVBoxLayout(self)
-        layout.addStretch()
-        layout.addWidget(QLabel("The Bricklet is not yet supported in this version of Brickv. Please Update Brickv."))
-        layout.addStretch()
+        self.qtcb_state_changed.connect(self.cb_state_changed)
+        self.button.register_callback(self.button.CALLBACK_STATE_CHANGED,
+                                      self.qtcb_state_changed.emit)
         
+        self.led1 = DualButton.OFF
+        self.led2 = DualButton.OFF
+        self.button1 = DualButton.RELEASED
+        self.button2 = DualButton.RELEASED
+        
+        self.button_led_on_b1.pressed.connect(self.on_b1_pressed)
+        self.button_led_on_b2.pressed.connect(self.on_b2_pressed)
+        self.button_led_off_b1.pressed.connect(self.off_b1_pressed)
+        self.button_led_off_b2.pressed.connect(self.off_b2_pressed)
+        self.button_toggle_b1.pressed.connect(self.toggle_b1_pressed)
+        self.button_toggle_b2.pressed.connect(self.toggle_b2_pressed)
+        
+    def on_b1_pressed(self):
+        self.led1 = DualButton.ON
+        self.button.set_led_state(DualButton.ON, self.led2)
+        self.update_buttons()
+    
+    def on_b2_pressed(self):
+        self.led2 = DualButton.ON
+        self.button.set_led_state(self.led1, DualButton.ON)
+        self.update_buttons()
+    
+    def off_b1_pressed(self):
+        self.led1 = DualButton.OFF
+        self.button.set_led_state(DualButton.OFF, self.led2)
+        self.update_buttons()
+    
+    def off_b2_pressed(self):
+        self.led2 = DualButton.OFF
+        self.button.set_led_state(self.led1, DualButton.OFF)
+        self.update_buttons()
+    
+    def toggle_b1_pressed(self):
+        self.led1 = DualButton.AT_OFF
+        self.button.set_led_state(DualButton.AT_OFF, self.led2)
+        self.update_buttons()
+    
+    def toggle_b2_pressed(self):
+        self.led2 = DualButton.AT_OFF
+        self.button.set_led_state(self.led1, DualButton.AT_OFF)
+        self.update_buttons()
+        
+    def update_buttons(self):
+        if self.led1 == DualButton.ON:
+            self.button_toggle_b1.setEnabled(True)
+            self.button_led_on_b1.setEnabled(False)
+            self.button_led_off_b1.setEnabled(True)
+        elif self.led1 == DualButton.OFF:
+            self.button_toggle_b1.setEnabled(True)
+            self.button_led_on_b1.setEnabled(True)
+            self.button_led_off_b1.setEnabled(False)
+        elif self.led1 == DualButton.AT_OFF:
+            self.button_toggle_b1.setEnabled(False)
+            self.button_led_on_b1.setEnabled(True)
+            self.button_led_off_b1.setEnabled(True)
+            
+        if self.led2 == DualButton.ON:
+            self.button_toggle_b2.setEnabled(True)
+            self.button_led_on_b2.setEnabled(False)
+            self.button_led_off_b2.setEnabled(True)
+        elif self.led2 == DualButton.OFF:
+            self.button_toggle_b2.setEnabled(True)
+            self.button_led_on_b2.setEnabled(True)
+            self.button_led_off_b2.setEnabled(False)
+        elif self.led2 == DualButton.AT_OFF:
+            self.button_toggle_b2.setEnabled(False)
+            self.button_led_on_b2.setEnabled(True)
+            self.button_led_off_b2.setEnabled(True)
+            
+    def cb_state_changed(self, button1, button2, led1, led2):
+        self.get_led_state_async((led1, led2))
+        self.get_button_state_async((button1, button2))
+        
+        self.update_buttons()
+    
+    def get_led_state_async(self, led):
+        self.led1, self.led2 = led
+    
+    def get_button_state_async(self, button):
+        self.button1, self.button2 = button
+        led_text_b1 = ''
+        led_text_b2 = ''
+        
+        if self.led1 in (DualButton.ON, DualButton.AT_ON):
+            led_text_b1 = ', LED On'
+        else:
+            led_text_b1 = ', LED Off'
+            
+        if self.led2 in (DualButton.ON, DualButton.AT_ON):
+            led_text_b2 = ', LED On'
+        else:
+            led_text_b2 = ', LED Off'
+            
+        if self.button1 == DualButton.RELEASED:
+            self.label_status_b1.setText('Released' + led_text_b1)
+        else:
+            self.label_status_b1.setText('Pressed' + led_text_b1)
+            
+        if self.button2 == DualButton.RELEASED:
+            self.label_status_b2.setText('Released' + led_text_b2)
+        else:
+            self.label_status_b2.setText('Pressed' + led_text_b2)
 
     def start(self):
-        pass
+        async_call(self.button.get_led_state, None, self.get_led_state_async, self.increase_error_count)
+        async_call(self.button.get_button_state, None, self.get_button_state_async, self.increase_error_count)
         
     def stop(self):
         pass
