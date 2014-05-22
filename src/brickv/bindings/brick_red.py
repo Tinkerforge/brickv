2b0e44896a29e4dccb5d18d205b8abc40599b95f
Matthias Bolte <matthias@tinkerforge.com>
2014-05-22 13:14:14 +0200

    Add basic RED Brick plugin

---
+++ src/brickv/bindings/brick_red.py
@@ -0,0 +1,63 @@
+# -*- coding: utf-8 -*-
+#############################################################
+# This file was automatically generated on 2014-05-21.      #
+#                                                           #
+# Bindings Version 2.1.0                                    #
+#                                                           #
+# If you have a bugfix for this file and want to commit it, #
+# please fix the bug in the generator. You can find a link  #
+# to the generator git on tinkerforge.com                   #
+#############################################################
+
+try:
+    from collections import namedtuple
+except ImportError:
+    try:
+        from .ip_connection import namedtuple
+    except ValueError:
+        from ip_connection import namedtuple
+
+try:
+    from .ip_connection import Device, IPConnection, Error
+except ValueError:
+    from ip_connection import Device, IPConnection, Error
+
+GetIdentity = namedtuple('Identity', ['uid', 'connected_uid', 'position', 'hardware_version', 'firmware_version', 'device_identifier'])
+
+class BrickRED(Device):
+    """
+    Device for running user programs standalone on the stack
+    """
+
+    DEVICE_IDENTIFIER = 17
+
+
+    FUNCTION_GET_IDENTITY = 255
+
+
+    def __init__(self, uid, ipcon):
+        """
+        Creates an object with the unique device ID *uid* and adds it to
+        the IP Connection *ipcon*.
+        """
+        Device.__init__(self, uid, ipcon)
+
+        self.api_version = (2, 0, 0)
+
+        self.response_expected[BrickRED.FUNCTION_GET_IDENTITY] = BrickRED.RESPONSE_EXPECTED_ALWAYS_TRUE
+
+
+    def get_identity(self):
+        """
+        Returns the UID, the UID where the Brick is connected to, 
+        the position, the hardware and firmware version as well as the
+        device identifier.
+        
+        The position can be '0'-'8' (stack position).
+        
+        The device identifier numbers can be found :ref:`here <device_identifier>`.
+        |device_identifier_constant|
+        """
+        return GetIdentity(*self.ipcon.send_request(self, BrickRED.FUNCTION_GET_IDENTITY, (), '', '8s 8s c 3B 3B H'))
+
+RED = BrickRED # for backward compatibility
---
+++ src/brickv/plugin_system/plugins/red/red.py
@@ -0,0 +1,62 @@
+# -*- coding: utf-8 -*-
+"""
+RED Plugin
+Copyright (C) 2014 Matthias Bolte <matthias@tinkerforge.com>
+
+red.py: RED Plugin implementation
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+General Public License for more details.
+
+You should have received a copy of the GNU General Public
+License along with this program; if not, write to the
+Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+Boston, MA 02111-1307, USA.
+"""
+
+from brickv.plugin_system.plugin_base import PluginBase
+from brickv.bindings.brick_red import BrickRED
+from brickv.async_call import async_call
+
+from brickv.plugin_system.plugins.red.ui_red import Ui_RED
+
+class RED(PluginBase, Ui_RED):
+    def __init__(self, ipcon, uid, version):
+        PluginBase.__init__(self, ipcon, uid, 'RED Brick', version)
+
+        self.setupUi(self)
+
+        self.red = BrickRED(uid, ipcon)
+        self.device = self.red
+
+    def start(self):
+        pass
+
+    def stop(self):
+        pass
+
+    def destroy(self):
+        self.destroy_ui()
+
+    def has_reset_device(self):
+        return False # FIXME: will have reboot, instead of reset
+
+    def reset_device(self):
+        pass
+
+    def is_brick(self):
+        return True
+
+    def get_url_part(self):
+        return 'red'
+
+    @staticmethod
+    def has_device_identifier(device_identifier):
+        return device_identifier == BrickRED.DEVICE_IDENTIFIER
