ef8281e58cd7a21dc5610392eb56fdcfe63565c3
Olaf Lüke <olaf@tinkerforge.com>
2014-10-13 22:52:37 +0200

    Use "gathering data..." on focus

--- src/brickv/plugin_system/plugins/red/red_tab_overview.py
+++ src/brickv/plugin_system/plugins/red/red_tab_overview.py
@@ -64,6 +64,8 @@ class REDTabOverview(QtGui.QWidget, Ui_REDTabOverview):
             (self.cb_tview_process_sort_indicator_changed)
 
     def tab_on_focus(self):
+        self.button_refresh.setText('Gathering data...')
+        self.button_refresh.setDisabled(True)
         self.is_tab_on_focus = True
         self.script_manager.execute_script('overview',
                                            self.cb_state_changed,
