295325bad10409e5bdeee88b370651f3b7e8a44e
Olaf Lüke <olaf@tinkerforge.com>
2014-10-15 23:04:40 +0200

    Add working example for reading brickd with config_parser

--- src/brickv/plugin_system/plugins/red/red_tab_settings.py
+++ src/brickv/plugin_system/plugins/red/red_tab_settings.py
@@ -21,23 +21,39 @@ Free Software Foundation, Inc., 59 Temple Place - Suite 330,
 Boston, MA 02111-1307, USA.
 """
 
-#spinBox.setDisplayIntegerBase(16)
-
 from PyQt4 import Qt, QtCore, QtGui
 from brickv.plugin_system.plugins.red.ui_red_tab_settings import Ui_REDTabSettings
 from brickv.plugin_system.plugins.red.api import *
+from brickv.plugin_system.plugins.red import config_parser
+
+from brickv.async_call import async_call
 
 class REDTabSettings(QtGui.QWidget, Ui_REDTabSettings):
+    script_manager = None
     def __init__(self):
         QtGui.QWidget.__init__(self)
         self.setupUi(self)
 
         self.session = None
+        self.brickd_conf = None
 
     def tab_on_focus(self):
-        pass
+        self.brickd_conf = REDFile(self.session)
+        async_call(self.brickd_conf.open, ('/etc/brickd.conf', REDFile.FLAG_READ_ONLY | REDFile.FLAG_NON_BLOCKING, 0, 0, 0), self.cb_brickd_conf_open, self.cb_brickd_conf_open_error)
 
     def tab_off_focus(self):
         pass
 
     # the callbacks
+    def cb_brickd_conf_open_error(self):
+        print "Could not open brickd.conf"
+        
+    def cb_brickd_conf_open(self, result):
+        self.brickd_conf.read_async(4096, self.cb_read_brickd_conf)
+        
+    def cb_read_brickd_conf(self, result):
+        if result == None:
+            print "Could not read brickd.conf"
+            
+        print config_parser.parse(result.data)
+        self.brickd_conf.release()
