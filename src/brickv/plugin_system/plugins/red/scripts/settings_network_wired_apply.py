36c4c8ef9b22b25b0a2ee26273d99f550bde1b7b
Ishraq Ibne Ashraf <ishraq@tinkerforge.com>
2014-10-27 11:52:34 +0100

    red-brick: Sequence wired connection config activation

--- src/brickv/plugin_system/plugins/red/scripts/settings_network_wired_apply.py
+++ src/brickv/plugin_system/plugins/red/scripts/settings_network_wired_apply.py
@@ -2,8 +2,6 @@
 
 import subprocess
 
-cmd_restart_wicd = "/etc/init.d/wicd force-reload"
-cmd_connect_wired = "/usr/bin/wicd-cli --wired -n0 -c"
+cmd = "/etc/init.d/wicd force-reload && /usr/bin/wicd-cli --wired -n0 -c"
 
-subprocess.Popen(cmd_restart_wicd, shell=True)
-subprocess.Popen(cmd_connect_wired, shell=True)
+subprocess.Popen(cmd, shell=True)
