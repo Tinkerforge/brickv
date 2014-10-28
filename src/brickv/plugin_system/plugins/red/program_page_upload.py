ca61e995be9e2f7b7f1a5e36c43c26ab2f27b07a
Olaf Lüke <olaf@tinkerforge.com>
2014-10-28 19:45:59 +0100

    Fix merge typo

--- src/brickv/plugin_system/plugins/red/program_page_upload.py
+++ src/brickv/plugin_system/plugins/red/program_page_upload.py
@@ -242,7 +242,7 @@ class ProgramPageUpload(ProgramPage, Ui_ProgramPageUpload):
         elif self.language == 'perl':
             executable, arguments, environment, working_directory = self.wizard().page(Constants.PAGE_PERL).get_command()
         elif self.language == 'php':
-            executable, arguments, environment working_directory = self.wizard().page(Constants.PAGE_PHP).get_command()
+            executable, arguments, environment, working_directory = self.wizard().page(Constants.PAGE_PHP).get_command()
         elif self.language == 'python':
             executable, arguments, environment, working_directory = self.wizard().page(Constants.PAGE_PYTHON).get_command()
         elif self.language == 'ruby':
