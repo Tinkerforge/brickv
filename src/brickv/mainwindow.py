f1cc903d963f1b54e1572218aa035d4515ca8df8
Matthias Bolte <matthias@tinkerforge.com>
2014-11-21 14:37:07 +0100

    Remove unused reconnect slot

--- src/brickv/mainwindow.py
+++ src/brickv/mainwindow.py
@@ -22,7 +22,7 @@ Free Software Foundation, Inc., 59 Temple Place - Suite 330,
 Boston, MA 02111-1307, USA.
 """
 
-from PyQt4.QtCore import pyqtSignal, QVariant, Qt, QTimer, QEvent, pyqtSlot
+from PyQt4.QtCore import pyqtSignal, QVariant, Qt, QTimer, QEvent
 from PyQt4.QtGui import QApplication, QMainWindow, QMessageBox, \
                         QPushButton, QWidget, QHBoxLayout, QVBoxLayout, \
                         QLabel, QFrame, QSpacerItem, QSizePolicy, \
@@ -148,6 +148,7 @@ class MainWindow(QMainWindow, Ui_MainWindow):
         self.label_auto_reconnects.hide()
         self.auto_reconnects = 0
 
+    # override QMainWindow.closeEvent
     def closeEvent(self, event):
         self.exit_brickv()
 
@@ -350,13 +351,6 @@ class MainWindow(QMainWindow, Ui_MainWindow):
 
         self.update_advanced_window()
         self.advanced_window.show()
-
-    @pyqtSlot()
-    def reconnect(self):
-        time.sleep(0.5)
-        self.connect_pressed()
-        time.sleep(0.5)
-        self.connect_pressed()
 
     def connect_pressed(self):
         if self.ipcon.get_connection_state() == IPConnection.CONNECTION_STATE_DISCONNECTED:
