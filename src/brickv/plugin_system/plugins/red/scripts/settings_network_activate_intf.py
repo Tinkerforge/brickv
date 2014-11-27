d6a45561218b629e31d7ecba2d1509244470a025
Ishraq Ibne Ashraf <ishraq@tinkerforge.com>
2014-11-27 14:22:10 +0100

    red-brick: Bring interface up before reloading wicd configuration and return non zero exit code in case of error

--- src/brickv/plugin_system/plugins/red/scripts/settings_network_activate_intf.py
+++ src/brickv/plugin_system/plugins/red/scripts/settings_network_activate_intf.py
@@ -11,11 +11,15 @@ iname = unicode(argv[1])
 itype = unicode(argv[2])
 
 if itype == 'wireless':
-    cmd_disconnect_restart = "/sbin/ifconfig "+iname+" down; "+"/usr/bin/wicd-cli --wireless -x; /etc/init.d/wicd force-reload"
-    ps = subprocess.Popen(cmd_disconnect_restart, shell=True)
+    cmd_wireless = "/sbin/ifconfig "+iname+" up; "+"/usr/bin/wicd-cli --wireless -x; /etc/init.d/wicd force-reload"
+    ps = subprocess.Popen(cmd_wireless, shell=True)
     comm = ps.communicate()
+    if cmd_disconnect_restart.returncode:
+        exit(1)
 elif itype == 'wired':
-    cmd_disconnect_restart_connect = "/sbin/ifconfig "+iname+" down; "+"/usr/bin/wicd-cli --wired -x; \
+    cmd_wired = "/sbin/ifconfig "+iname+" up; "+"/usr/bin/wicd-cli --wired -x; \
     /etc/init.d/wicd force-reload; /usr/bin/wicd-cli --wired -c"
-    ps = subprocess.Popen(cmd_disconnect_restart_connect, shell=True)
+    ps = subprocess.Popen(cmd_wired, shell=True)
     comm = ps.communicate()
+    if cmd_disconnect_restart.returncode:
+        exit(1)
