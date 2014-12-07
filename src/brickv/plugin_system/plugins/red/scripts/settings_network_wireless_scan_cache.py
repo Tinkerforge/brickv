05c66364a656b5256b793c06e550a4f43ad56c28
Ishraq Ibne Ashraf <ishraq@tinkerforge.com>
2014-12-07 08:57:42 +0100

    red-brick: Dbus based wireless cache results fetch.

--- src/brickv/plugin_system/plugins/red/scripts/settings_network_wireless_scan_cache.py
+++ src/brickv/plugin_system/plugins/red/scripts/settings_network_wireless_scan_cache.py
@@ -2,41 +2,39 @@
 # -*- coding: utf-8 -*-
 
 import json
+import dbus
 import subprocess
 
 return_dict = None
 
-wlscanc_cmd = '/usr/bin/wicd-cli --wireless -l && :'
-wlscanc_ps = subprocess.Popen(wlscanc_cmd, shell=True, stdout=subprocess.PIPE)
-wlscanc_output = wlscanc_ps.communicate()[0]
-if wlscanc_ps.returncode:
+try:
+    bus = dbus.SystemBus()
+    wireless = dbus.Interface(bus.get_object('org.wicd.daemon', '/org/wicd/daemon/wireless'),
+                                             'org.wicd.daemon.wireless')
+except:
     exit(1)
 
-_lines = wlscanc_output.splitlines()
+return_dict = {}
 
-if wlscanc_output and len(_lines) > 0:
-    return_dict = {}
-    for l in _lines:
-        lsplitted = l.split('\t')
-        if len(lsplitted) == 1:
-            continue
-        _apdict = {'essid': lsplitted[0],
-                   'bssid': lsplitted[1],
-                   'channel': lsplitted[2]}
-        return_dict[lsplitted[3]] = _apdict
+for network_id in range(0, wireless.GetNumberOfNetworks()):
+    try:
+        essid = wireless.GetWirelessProperty(network_id, 'essid')
+        bssid = wireless.GetWirelessProperty(network_id, 'bssid')
+        channel = wireless.GetWirelessProperty(network_id, 'channel')
+        if wireless.GetWirelessProperty(network_id, "encryption"):
+            encryption = 'On'
+            encryption_method = wireless.GetWirelessProperty(network_id, "encryption_method")
+        else:
+            encryption = 'Off'
+            encryption_method = None
 
-    for key, value in return_dict.iteritems():
-        netdetail_cmd = '/usr/bin/wicd-cli --wireless -n'+key+' -d'
-        netdetail_ps = subprocess.Popen(netdetail_cmd, shell=True, stdout=subprocess.PIPE)
-        netdetail_output = netdetail_ps.communicate()[0]
-        if netdetail_ps.returncode:
-            exit(1)
-        lines = netdetail_output.split('\n')
-        for l in lines:
-            lsplitted = l.split(': ')
-            if lsplitted[0] == 'Encryption':
-                return_dict[key]['encryption'] = lsplitted[1]
-            elif lsplitted[0] == 'Encryption Method':
-                return_dict[key]['encryption_method'] = lsplitted[1]
+        _apdict = {'essid':essid,
+                   'bssid':bssid,
+                   'channel':channel,
+                   'encryption':encryption,
+                   'encryption_method':encryption_method}
+        return_dict[network_id] = _apdict
+    except:
+        pass
 
 print json.dumps(return_dict, separators=(',', ':'))
