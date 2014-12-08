ebe093ce6cef265b493374e73bfd0008249bb07e
Ishraq Ibne Ashraf <ishraq@tinkerforge.com>
2014-12-08 12:51:44 +0100

    red-brick: Workaround to ignore failure in trying to put an interface down which is not present.

--- src/brickv/plugin_system/plugins/red/scripts/settings_network_apply.py
+++ src/brickv/plugin_system/plugins/red/scripts/settings_network_apply.py
@@ -11,31 +11,24 @@ iname = unicode(argv[1])
 iname_previous = unicode(argv[2])
 itype = unicode(argv[3])
 
+if iname_previous and iname_previous != 'None':
+    cmd_ifdown = '/sbin/ifconfig '+iname_previous+' down 2> /dev/null && /bin/sleep 1'
+    os.system(cmd_ifdown)
+
 if itype == 'wireless':
     netidx = unicode(argv[4])
-    if iname_previous and iname_previous != 'None':
-        cmd_wireless = '/usr/bin/wicd-cli --wireless -x && /bin/sleep 5 &&\
-                        /sbin/ifconfig '+iname_previous+' down && /bin/sleep 1;\
-                        /usr/sbin/service wicd restart && /bin/sleep 5 &&\
-                        /usr/bin/wicd-cli --wireless -c -n'+netidx+' && :'
-    else:
-        cmd_wireless = '/usr/bin/wicd-cli --wireless -x && /bin/sleep 5 &&\
-                        /usr/sbin/service wicd restart && /bin/sleep 5 &&\
-                        /usr/bin/wicd-cli --wireless -c -n'+netidx+' && :'
+
+    cmd_wireless = '/usr/bin/wicd-cli --wireless -x && /bin/sleep 5 &&\
+                    /usr/sbin/service wicd restart && /bin/sleep 5 &&\
+                    /usr/bin/wicd-cli --wireless -c -n'+netidx+' && :'
     cmd_wireless_code = os.system(cmd_wireless)
     if cmd_wireless_code:
         exit (1)
 
 elif itype == 'wired':
-    if iname_previous and iname_previous != 'None':
-        cmd_wired = '/usr/bin/wicd-cli --wired -x && /bin/sleep 5 &&\
-                     /sbin/ifconfig '+iname_previous+' down && /bin/sleep 1;\
-                     /usr/sbin/service wicd restart && /bin/sleep 5 &&\
-                     /usr/bin/wicd-cli --wired -c -n0 && :'
-    else:
-        cmd_wired = '/usr/bin/wicd-cli --wired -x && /bin/sleep 5 &&\
-                     /usr/sbin/service wicd restart && /bin/sleep 5 &&\
-                     /usr/bin/wicd-cli --wired -c -n0 && :'
+    cmd_wired = '/usr/bin/wicd-cli --wired -x && /bin/sleep 5 &&\
+                 /usr/sbin/service wicd restart && /bin/sleep 5 &&\
+                 /usr/bin/wicd-cli --wired -c -n0 && :'
     cmd_wired_code = os.system(cmd_wired)
     if cmd_wired_code:
         exit (1)
