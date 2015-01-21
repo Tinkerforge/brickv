b5b041c7fda5853acd8b62d431affc86a81e3464
Matthias Bolte <matthias@tinkerforge.com>
2015-01-21 16:29:43 +0100

    red-brick: No need to set PATH in the compile helper scripts
    The script manager already sets PATH properly.

--- src/brickv/plugin_system/plugins/red/scripts/fpcmake_helper.sh
+++ src/brickv/plugin_system/plugins/red/scripts/fpcmake_helper.sh
@@ -1,7 +1,6 @@
 #!/bin/sh -e
 
-export PATH=/usr/bin:/bin:/usr/sbin:/sbin:/usr/local/bin
-export FPCDIR=/usr/lib/fpc/`ls /usr/lib/fpc/ | grep -E [0-9].[0-9].[0-9] | head -n1`
+export FPCDIR=/usr/lib/fpc/default
 
 cd $1
 shift
--- src/brickv/plugin_system/plugins/red/scripts/make_helper.sh
+++ src/brickv/plugin_system/plugins/red/scripts/make_helper.sh
@@ -1,7 +1,5 @@
 #!/bin/sh -e
 
-export PATH=/usr/bin:/bin:/usr/sbin:/sbin:/usr/local/bin
-
 cd $1
 shift
 make $@
