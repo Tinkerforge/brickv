828d95685c15c83bddd7a29d0ec3a842878c6a59
Olaf Lüke <olaf@tinkerforge.com>
2015-03-03 14:10:48 +0100

    Add GUI for detector type and heater

--- src/brickv/plugin_system/plugins/gas_detector/gas_detector.py
+++ src/brickv/plugin_system/plugins/gas_detector/gas_detector.py
@@ -26,7 +26,7 @@ from brickv.plot_widget import PlotWidget
 from brickv.bindings.bricklet_gas_detector import BrickletGasDetector
 from brickv.async_call import async_call
 
-from PyQt4.QtGui import QVBoxLayout, QLabel, QHBoxLayout
+from PyQt4.QtGui import QVBoxLayout, QLabel, QHBoxLayout, QCheckBox, QComboBox
 from PyQt4.QtCore import pyqtSignal, Qt
 
 class GasDetectorLabel(QLabel):
@@ -52,25 +52,62 @@ class GasDetector(PluginBase):
         plot_list = [['', Qt.red, self.get_current_value]]
         self.plot_widget = PlotWidget('Value', plot_list)
         
+        
+        self.heater_checkbox = QCheckBox()
+        self.heater_checkbox.setText('Heater')
+        
+        self.type_combo = QComboBox()
+        self.type_combo.addItem('Type 0 (MQ2, MQ5)')
+        self.type_combo.addItem('Type 1 (MQ3)')
+        
+        self.type_combo.currentIndexChanged.connect(self.type_combo_index_changed)
+        self.heater_checkbox.stateChanged.connect(self.heater_checkbox_state_changed)
+        
         layout_h = QHBoxLayout()
         layout_h.addStretch()
         layout_h.addWidget(self.gas_detector_label)
         layout_h.addStretch()
+        
+        layout_h1 = QHBoxLayout()
+        layout_h1.addWidget(QLabel('Detector Type:'))
+        layout_h1.addWidget(self.type_combo)
+        layout_h1.addStretch()
+        layout_h1.addWidget(self.heater_checkbox)
 
         layout = QVBoxLayout(self)
         layout.addLayout(layout_h)
         layout.addWidget(self.plot_widget)
+        layout.addLayout(layout_h1)
         
+    def heater_checkbox_state_changed(self, state):
+        if state == Qt.Unchecked:
+            self.gas_detector.heater_off()
+        elif state == Qt.Checked:
+            self.gas_detector.heater_on()
+        
+    def type_combo_index_changed(self, index):
+        async_call(self.gas_detector.set_detector_type, index, None, self.increase_error_count)
+
+    def get_detector_type_async(self, detector_type):
+        self.type_combo.setCurrentIndex(detector_type)
+        
+    def is_heater_on_async(self, heater):
+        if heater:
+            self.heater_checkbox.setChecked(True)
+        else:
+            self.heater_checkbox.setChecked(False)
         
     def get_current_value(self):
         return self.current_value
 
     def cb_value(self, value):
-        print value
         self.current_value = value
         self.gas_detector_label.setText(str(value))
 
     def start(self):
+        async_call(self.gas_detector.get_detector_type, None, self.get_detector_type_async, self.increase_error_count)
+        async_call(self.gas_detector.is_heater_on, None, self.is_heater_on_async, self.increase_error_count)
+        
         async_call(self.gas_detector.get_value, None, self.cb_value, self.increase_error_count)
         async_call(self.gas_detector.set_value_callback_period, 100, None, self.increase_error_count)
         
