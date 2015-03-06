1fa5fc503d999b5fde8d3dabdc213d0a71344bbf
Matthias Bolte <matthias@tinkerforge.com>
2015-03-06 18:43:11 +0100

    Don't run build_ui.py for servo plugin twice

--- src/brickv/build_ui.py
+++ src/brickv/build_ui.py
@@ -7,5 +7,3 @@ os.system("pyuic4 -o ui_mainwindow.py ui/brickv.ui")
 os.system("pyuic4 -o ui_flashing.py ui/flashing.ui")
 os.system("pyuic4 -o ui_advanced.py ui/advanced.ui")
 
-# Servo Brick
-os.system("pyuic4 -o plugin_system/plugins/servo/ui_servo.py plugin_system/plugins/servo/ui/servo.ui")
