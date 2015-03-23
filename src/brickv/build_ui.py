d84d21c94aee1253e4f5a38da4e3bc76f08a4303
Matthias Bolte <matthias@tinkerforge.com>
2015-03-23 12:58:10 +0100

    Rename brickv.ui to mainwindow.ui

--- src/brickv/build_ui.py
+++ src/brickv/build_ui.py
@@ -6,6 +6,6 @@ def system(command):
     if os.system(command) != 0:
         exit(1)
 
-system("pyuic4 -o ui_mainwindow.py ui/brickv.ui")
+system("pyuic4 -o ui_mainwindow.py ui/mainwindow.ui")
 system("pyuic4 -o ui_flashing.py ui/flashing.ui")
 system("pyuic4 -o ui_advanced.py ui/advanced.ui")
