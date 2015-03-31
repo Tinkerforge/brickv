6c7dbc290ec1cc566f5512d092119fd4f0bfa90c
Olaf Lüke <olaf@tinkerforge.com>
2015-03-31 13:24:33 +0200

    Add stub for Industrial Analog Out Bricklet

--- src/brickv/plugin_system/plugins/__init__.py
+++ src/brickv/plugin_system/plugins/__init__.py
@@ -20,6 +20,7 @@ from brickv.plugin_system.plugins.hall_effect import device_class as hall_effect
 from brickv.plugin_system.plugins.heart_rate import device_class as heart_rate
 from brickv.plugin_system.plugins.humidity import device_class as humidity
 from brickv.plugin_system.plugins.imu import device_class as imu
+from brickv.plugin_system.plugins.industrial_analog_out import device_class as industrial_analog_out
 from brickv.plugin_system.plugins.industrial_digital_in_4 import device_class as industrial_digital_in_4
 from brickv.plugin_system.plugins.industrial_digital_out_4 import device_class as industrial_digital_out_4
 from brickv.plugin_system.plugins.industrial_dual_0_20ma import device_class as industrial_dual_0_20ma
@@ -82,6 +83,7 @@ device_classes = [
     heart_rate,
     humidity,
     imu,
+    industrial_analog_out,
     industrial_digital_in_4,
     industrial_digital_out_4,
     industrial_dual_0_20ma,
