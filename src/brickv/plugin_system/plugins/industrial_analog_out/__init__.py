cd36974e84fe191867acaea0a00ae0af16d042b1
Olaf Lüke <olaf@tinkerforge.com>
2015-03-31 13:35:17 +0200

    Fix typo

--- src/brickv/plugin_system/plugins/industrial_analog_out/__init__.py
+++ src/brickv/plugin_system/plugins/industrial_analog_out/__init__.py
@@ -21,6 +21,6 @@ Free Software Foundation, Inc., 59 Temple Place - Suite 330,
 Boston, MA 02111-1307, USA.
 """
 
-from brickv.plugin_system.plugins.instustrial_analog_out.industrial_analog_out import IndustrialAnalogOut
+from brickv.plugin_system.plugins.industrial_analog_out.industrial_analog_out import IndustrialAnalogOut
 
 device_class = IndustrialAnalogOut
