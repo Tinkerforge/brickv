3794449bfefc25d416119ce5581252f600a156c6
Matthias Bolte <matthias@tinkerforge.com>
2015-05-05 15:08:08 +0200

    load-cell: Move calibration to its own dialog

--- src/brickv/plugin_system/plugins/load_cell/load_cell.py
+++ src/brickv/plugin_system/plugins/load_cell/load_cell.py
@@ -24,10 +24,11 @@ Boston, MA 02111-1307, USA.
 
 from PyQt4.QtCore import Qt
 from PyQt4.QtGui import QVBoxLayout, QLabel, QHBoxLayout, QSpinBox, \
-                        QPushButton, QFrame, QComboBox, QCheckBox
+                        QPushButton, QFrame, QComboBox, QCheckBox, QDialog
 
 from brickv.plugin_system.plugin_base import PluginBase
 from brickv.bindings.bricklet_load_cell import BrickletLoadCell
+from brickv.plugin_system.plugins.load_cell.ui_calibration import Ui_Calibration
 from brickv.plot_widget import PlotWidget
 from brickv.async_call import async_call
 from brickv.callback_emulator import CallbackEmulator
@@ -39,12 +40,41 @@ class WeightLabel(QLabel):
         else:
             text = "Weight: " + "{0:.3f}".format(round(weight/1000.0, 3)) + " kg"
         super(WeightLabel, self).setText(text)
+
+class Calibration(QDialog, Ui_Calibration):
+    def __init__(self, parent):
+        QDialog.__init__(self, parent)
+
+        self.setupUi(self)
+
+        self.parent = parent
+        self.lc = parent.lc
+
+        self.button_zero.clicked.connect(self.button_zero_clicked)
+        self.button_weight.clicked.connect(self.button_weight_clicked)
+        self.button_weight.setEnabled(False)
         
-def line():
-    f = QFrame()
-    f.setFrameShape(QFrame.HLine)
-    f.setFrameShadow(QFrame.Sunken)
-    return f
+        self.label_step1.setStyleSheet('QLabel { color : red }')
+        self.label_step2.setStyleSheet('')
+        self.label_status.setText('Calibration in progress...')
+
+    def button_zero_clicked(self):
+        self.lc.calibrate(0)
+        self.button_weight.setEnabled(True)
+
+        self.label_step1.setStyleSheet('')
+        self.label_step2.setStyleSheet('QLabel { color : red }')
+
+    def button_weight_clicked(self):
+        self.lc.calibrate(self.spin_weight.value())
+
+        self.label_step1.setStyleSheet('')
+        self.label_step2.setStyleSheet('')
+        self.label_status.setText('The new calibration is now saved in the EEPROM.')
+
+    def closeEvent(self, event):
+        self.parent.button_calibration.setEnabled(True)
+        self.parent.calibration = None
     
 class LoadCell(PluginBase):
     def __init__(self, *args):
@@ -58,34 +88,18 @@ class LoadCell(PluginBase):
 
         self.weight_label = WeightLabel()
         
+        self.gain = 0 # 128x
         self.current_value = None
+        self.calibration = None
         
         plot_list = [['', Qt.red, self.get_current_value]]
         self.plot_widget = PlotWidget('Weight [g]', plot_list)
-        
-        self.button_tare = QPushButton("Tare")
-        self.button_zero = QPushButton("Calibrate Zero")
-        self.button_calibrate = QPushButton("Calibrate Weight")
-        self.spin_calibrate = QSpinBox()
-        self.spin_calibrate.setMinimum(100)
-        self.spin_calibrate.setMaximum(1000*1000*1000) # 1 ton
-        self.spin_calibrate.setSingleStep(100)
-        self.label_average = QLabel('Current Weight (g):')
 
-        self.label_calibrate = QLabel('To calibrate your Load Cell Bricklet you have to\n 1) Empty the scale and press "Calibrate Zero".\n 2) Add a known weight to the scale, enter it as "Current Weight" and press "Calibrate Weight".\nThe calibration is saved on the EEPROM and only has to be done once.')
-        
-        self.button_tare.pressed.connect(self.button_tare_pressed)
-        self.button_zero.pressed.connect(self.button_zero_pressed)
-        self.button_calibrate.pressed.connect(self.button_calibrate_pressed)
-        layout_cal1 = QHBoxLayout()
-        layout_cal1.addWidget(self.button_zero)
-        layout_cal1.addStretch()
-        
-        layout_cal2 = QHBoxLayout()
-        layout_cal2.addWidget(self.button_calibrate)
-        layout_cal2.addWidget(self.label_average)
-        layout_cal2.addWidget(self.spin_calibrate)
-        layout_cal2.addStretch()
+        self.button_calibration = QPushButton("Calibration")
+        self.button_calibration.clicked.connect(self.button_calibration_clicked)
+
+        self.button_tare = QPushButton("Tare")
+        self.button_tare.clicked.connect(self.button_tare_clicked)
         
         self.enable_led = QCheckBox("LED On")
         self.enable_led.stateChanged.connect(self.enable_led_changed)
@@ -97,23 +111,11 @@ class LoadCell(PluginBase):
         self.spin_average.setValue(5)
         self.spin_average.editingFinished.connect(self.spin_average_finished)
         self.label_average = QLabel('Length of moving average:')
-        layout_avg = QHBoxLayout()
-        layout_avg.addWidget(self.label_average)
-        layout_avg.addWidget(self.spin_average)
-        layout_avg.addStretch()
-        layout_avg.addWidget(self.enable_led)
-        
+
         layout_h = QHBoxLayout()
         layout_h.addStretch()
         layout_h.addWidget(self.weight_label)
         layout_h.addStretch()
-        
-        self.gain_label = QLabel('Gain: ')
-        self.gain_combo = QComboBox()
-        self.gain_combo.addItem("128x")
-        self.gain_combo.addItem("64x")
-        self.gain_combo.addItem("32x")
-        self.gain_combo.activated.connect(self.new_config)
         
         self.rate_label = QLabel('Rate: ')
         self.rate_combo = QComboBox()
@@ -121,26 +123,20 @@ class LoadCell(PluginBase):
         self.rate_combo.addItem("80Hz")
         self.rate_combo.activated.connect(self.new_config)
         
-        self.label_configuration = QLabel('Increasing the rate will increase the samples per second but also increase the noise on the data.\nChanging the gain is only necessary if your load cell is out of range. 128x is most likely OK.\nThe Gain and Rate configuration is also saved on the EEPROM and only has to be done once.')
-        
-        layout_hc = QHBoxLayout()
-        layout_hc.addWidget(self.rate_label)
-        layout_hc.addWidget(self.rate_combo)
-        layout_hc.addWidget(self.gain_label)
-        layout_hc.addWidget(self.gain_combo)
-        layout_hc.addStretch()
-       
+        layout_avg = QHBoxLayout()
+        layout_avg.addWidget(self.label_average)
+        layout_avg.addWidget(self.spin_average)
+        layout_avg.addWidget(self.rate_label)
+        layout_avg.addWidget(self.rate_combo)
+        layout_avg.addStretch()
+        layout_avg.addWidget(self.button_calibration)
+        layout_avg.addWidget(self.button_tare)
+        layout_avg.addWidget(self.enable_led)
+
         layout = QVBoxLayout(self)
         layout.addLayout(layout_h)
         layout.addWidget(self.plot_widget)
-        layout.addWidget(self.button_tare)
         layout.addLayout(layout_avg)
-        layout.addWidget(line())
-        layout.addWidget(self.label_calibrate)
-        layout.addLayout(layout_cal1)
-        layout.addLayout(layout_cal2)
-        layout.addWidget(self.label_configuration)
-        layout.addLayout(layout_hc)
 
     def start(self):
         async_call(self.lc.is_led_on, None, self.is_led_on_async, self.increase_error_count)
@@ -157,7 +153,8 @@ class LoadCell(PluginBase):
         self.plot_widget.stop = True
 
     def destroy(self):
-        pass
+        if self.calibration:
+            self.calibration.close()
 
     def get_url_part(self):
         return 'load_cell'
@@ -170,7 +167,7 @@ class LoadCell(PluginBase):
         self.spin_average.setValue(avg)
         
     def get_configuration_async(self, conf):
-        self.gain_combo.setCurrentIndex(conf.gain)
+        self.gain = conf.gain
         self.rate_combo.setCurrentIndex(conf.rate)
         
     def is_led_on_async(self, value):
@@ -178,15 +175,16 @@ class LoadCell(PluginBase):
             self.enable_led.setChecked(True)
         else:
             self.enable_led.setChecked(False)
-            
-    def button_tare_pressed(self):
-        self.lc.tare()
 
-    def button_zero_pressed(self):
-        self.lc.calibrate(0)
-        
-    def button_calibrate_pressed(self):
-        self.lc.calibrate(self.spin_calibrate.value())
+    def button_calibration_clicked(self):
+        if self.calibration is None:
+            self.calibration = Calibration(self)
+
+        self.button_calibration.setEnabled(False)
+        self.calibration.show()
+            
+    def button_tare_clicked(self):
+        self.lc.tare()
     
     def enable_led_changed(self, state):
         if state == Qt.Checked:
@@ -196,8 +194,7 @@ class LoadCell(PluginBase):
     
     def new_config(self, value):
         rate = self.rate_combo.currentIndex()
-        gain = self.gain_combo.currentIndex()
-        self.lc.set_configuration(rate, gain)
+        self.lc.set_configuration(rate, self.gain)
     
     def spin_average_finished(self):
         self.lc.set_moving_average(self.spin_average.value())
