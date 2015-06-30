28caacc528ed3bed043261a5e490784dec94ab32
Matthias Bolte <matthias@tinkerforge.com>
2015-06-30 17:02:51 +0200

    rs232: Handle non-ASCII chars as input

--- src/brickv/plugin_system/plugins/rs232/rs232.py
+++ src/brickv/plugin_system/plugins/rs232/rs232.py
@@ -1,4 +1,4 @@
-# -*- coding: utf-8 -*-  
+# -*- coding: utf-8 -*-
 """
 RS232 Plugin
 Copyright (C) 2015 Olaf Lüke <olaf@tinkerforge.com>
@@ -6,8 +6,8 @@ Copyright (C) 2015 Olaf Lüke <olaf@tinkerforge.com>
 rs232.py: RS232 Plugin Implementation
 
 This program is free software; you can redistribute it and/or
-modify it under the terms of the GNU General Public License 
-as published by the Free Software Foundation; either version 2 
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
 of the License, or (at your option) any later version.
 
 This program is distributed in the hope that it will be useful,
@@ -39,17 +39,17 @@ class RS232(PluginBase, Ui_RS232):
 
         self.setupUi(self)
         self.text.setReadOnly(True)
-        
+
         self.rs232 = self.device
-        
+
         self.qtcb_read.connect(self.cb_read)
         self.rs232.register_callback(self.rs232.CALLBACK_READ_CALLBACK,
-                                     self.qtcb_read.emit) 
-        
+                                     self.qtcb_read.emit)
+
         self.input_combobox.addItem("")
         self.input_combobox.lineEdit().setMaxLength(58)
         self.input_combobox.lineEdit().returnPressed.connect(self.input_changed)
-        
+
         self.baudrate_combobox.activated.connect(self.configuration_changed)
         self.parity_combobox.activated.connect(self.configuration_changed)
         self.stopbits_spinbox.editingFinished.connect(self.configuration_changed)
@@ -57,17 +57,17 @@ class RS232(PluginBase, Ui_RS232):
         self.hardware_flowcontrol_combobox.activated.connect(self.configuration_changed)
         self.software_flowcontrol_combobox.activated.connect(self.configuration_changed)
         self.text_type_combobox.activated.connect(self.text_type_changed)
-        
+
         self.hextext = QHexeditWidget(self.text.font())
         self.hextext.hide()
         self.layout().insertWidget(2, self.hextext)
-        
+
         self.save_button.clicked.connect(self.save_clicked)
 
     def cb_read(self, message, length):
         s = ''.join(message[:length])
         self.hextext.appendData(s)
-        
+
         # QTextEdit breaks lines at \r and \n
         s = s.replace('\n\r', '\n').replace('\r\n', '\n')
 
@@ -77,27 +77,27 @@ class RS232(PluginBase, Ui_RS232):
                 ascii += '.'
             else:
                 ascii += c
-        
+
         self.text.moveCursor(QTextCursor.End)
         self.text.insertPlainText(ascii)
         self.text.moveCursor(QTextCursor.End)
-            
+
     def input_changed(self):
-        text = str(self.input_combobox.currentText()) + '\n\r'
+        text = self.input_combobox.currentText().encode('utf-8') + '\n\r'
         c = ['\0']*60
         for i, t in enumerate(text):
             c[i] = t
-        
+
         length = len(text)
-        written = 0 
+        written = 0
         while length != 0:
             written = self.rs232.write(c, length)
-            c = c[written:] 
+            c = c[written:]
             c = c + ['\0']*written
             length = length - written
 
         self.input_combobox.setCurrentIndex(0)
-        
+
     def get_configuration_async(self, conf):
         self.baudrate_combobox.setCurrentIndex(conf.baudrate)
         self.parity_combobox.setCurrentIndex(conf.parity)
@@ -106,7 +106,7 @@ class RS232(PluginBase, Ui_RS232):
         self.hardware_flowcontrol_combobox.setCurrentIndex(conf.hardware_flowcontrol)
         self.software_flowcontrol_combobox.setCurrentIndex(conf.software_flowcontrol)
         self.save_button.setEnabled(False)
-        
+
     def text_type_changed(self):
         if self.text_type_combobox.currentIndex() == 0:
             self.hextext.hide()
@@ -114,10 +114,10 @@ class RS232(PluginBase, Ui_RS232):
         else:
             self.text.hide()
             self.hextext.show()
-        
+
     def configuration_changed(self):
         self.save_button.setEnabled(True)
-    
+
     def save_clicked(self):
         baudrate = self.baudrate_combobox.currentIndex()
         parity = self.parity_combobox.currentIndex()
@@ -125,15 +125,15 @@ class RS232(PluginBase, Ui_RS232):
         wordlength = self.wordlength_spinbox.value()
         hardware_flowcontrol = self.hardware_flowcontrol_combobox.currentIndex()
         software_flowcontrol = self.software_flowcontrol_combobox.currentIndex()
-        
+
         self.rs232.set_configuration(baudrate, parity, stopbits, wordlength, hardware_flowcontrol, software_flowcontrol)
         self.save_button.setEnabled(False)
-        
+
     def start(self):
         async_call(self.rs232.get_configuration, None, self.get_configuration_async, self.increase_error_count)
         self.rs232.enable_read_callback()
 #        self.cbe_read.set_period(100)
-        
+
     def stop(self):
         pass
 #        self.cbe_read.set_period(0)
