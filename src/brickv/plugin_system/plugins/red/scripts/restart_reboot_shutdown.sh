bd14b735a5894cdae800717df4ff354763d1e686
Ishraq Ibne Ashraf <ishraq@tinkerforge.com>
2015-07-25 21:29:59 +0200

    red-brick: Add condition for wrong supplied argument

--- src/brickv/plugin_system/plugins/red/scripts/restart_reboot_shutdown.sh
+++ src/brickv/plugin_system/plugins/red/scripts/restart_reboot_shutdown.sh
@@ -1,23 +1,24 @@
 #!/bin/sh
 
 if [ $# -eq 0 ]
-  then
+then
     echo "No arguments supplied"
     exit 1
-fi
 
-if [ $1 -eq 0 ]
-  then
+elif [ $1 -eq 0 ]
+then
     /etc/init.d/brickd restart
-fi
 
-if [ $1 -eq 1 ]
-  then
+elif [ $1 -eq 1 ]
+then
     init 6
 #   reboot
-fi
 
-if [ $1 -eq 2 ]
-  then
+elif [ $1 -eq 2 ]
+then
     halt
+
+else
+    echo "Wrong argument supplied"
+    exit 1
 fi
