bc4f7d82b7bbc930d666743d6a92fd162a7501b3
Olaf Lüke <olaf@tinkerforge.com>
2015-07-28 18:34:47 +0200

    Add GUI for Ind. Dual Analog In recalibration

--- src/brickv/plugin_system/plugins/industrial_dual_analog_in/industrial_dual_analog_in.py
+++ src/brickv/plugin_system/plugins/industrial_dual_analog_in/industrial_dual_analog_in.py
@@ -24,7 +24,7 @@ Boston, MA 02111-1307, USA.
 
 import functools
 
-from PyQt4.QtGui import QVBoxLayout, QLabel, QHBoxLayout, QComboBox
+from PyQt4.QtGui import QVBoxLayout, QLabel, QHBoxLayout, QComboBox, QPushButton, QFrame, QDialog
 from PyQt4.QtCore import Qt
 
 from brickv.plugin_system.plugin_base import PluginBase
@@ -32,11 +32,92 @@ from brickv.bindings.bricklet_industrial_dual_analog_in import BrickletIndustria
 from brickv.plot_widget import PlotWidget
 from brickv.async_call import async_call
 from brickv.callback_emulator import CallbackEmulator
+from brickv.plugin_system.plugins.industrial_dual_analog_in.ui_calibration import Ui_Calibration
 
 class VoltageLabel(QLabel):
     def setText(self, text):
         text = text + " V"
         super(VoltageLabel, self).setText(text)
+        
+        
+class Calibration(QDialog, Ui_Calibration):
+    def __init__(self, parent):
+        QDialog.__init__(self, parent)
+        self.parent = parent
+        
+        self.values0 = [0]*10
+        self.values1 = [0]*10
+        self.values_index = 0
+
+        self.setupUi(self)
+        
+        self.button_cal_remove.clicked.connect(self.remove_clicked)
+        self.button_cal_offset.clicked.connect(self.offset_clicked)
+        self.button_cal_gain.clicked.connect(self.gain_clicked)
+        
+        self.cbe_adc = CallbackEmulator(self.parent.analog_in.get_adc_values,
+                                        self.cb_adc_values,
+                                        self.parent.increase_error_count)
+
+    def show(self):
+        QDialog.show(self)
+                
+        self.cbe_adc.set_period(100)
+        
+        self.current_offset0 = 0
+        self.current_offset1 = 0
+        self.current_gain0 = 0
+        self.current_gain1 = 0
+        
+        self.update_calibration()
+  
+    def update_calibration(self):
+        async_call(self.parent.analog_in.get_calibration, None, self.cb_get_calibration, self.parent.increase_error_count)
+        
+    def remove_clicked(self):
+        self.parent.analog_in.set_calibration((0, 0), (0, 0))
+        self.update_calibration()
+        
+    def offset_clicked(self):
+        self.parent.analog_in.set_calibration((-sum(self.values0)/10, -sum(self.values1)/10), (self.current_gain0, self.current_gain1))
+        self.update_calibration()
+        
+    def gain_clicked(self):
+        measured0 = (sum(self.values0)/10.0)*244/38588
+        measured1 = (sum(self.values1)/10.0)*244/38588
+        factor0 = self.spinbox_voltage_ch0.value()/measured0
+        factor1 = self.spinbox_voltage_ch1.value()/measured1
+        gain0 = int((factor0-1)*2**23)
+        gain1 = int((factor1-1)*2**23)
+
+        self.parent.analog_in.set_calibration((self.current_offset0, self.current_offset1), (gain0, gain1))
+        self.update_calibration()
+        
+    def cb_get_calibration(self, cal):
+        self.current_offset0 = cal.offset[0]
+        self.current_offset1 = cal.offset[1]
+        self.current_gain0 = cal.gain[0]
+        self.current_gain1 = cal.gain[1]
+
+        self.label_offset0.setText(str(cal.offset[0]))
+        self.label_offset1.setText(str(cal.offset[1]))
+        self.label_gain0.setText(str(cal.gain[0]))
+        self.label_gain1.setText(str(cal.gain[1]))
+        
+    def cb_adc_values(self, values):
+        self.values0[self.values_index] = values[0]
+        self.values1[self.values_index] = values[1]
+
+        self.values_index += 1
+        if self.values_index >= 10:
+            self.values_index = 0
+        
+        self.label_adc0.setText(str(sum(self.values0)/10))
+        self.label_adc1.setText(str(sum(self.values1)/10))
+        
+    def closeEvent(self, event):
+        self.parent.calibration_button.setEnabled(True)
+        self.cbe_adc.set_period(0)
 
 class IndustrialDualAnalogIn(PluginBase):
     def __init__(self, *args):
@@ -53,6 +134,8 @@ class IndustrialDualAnalogIn(PluginBase):
 
         self.voltage_label = [VoltageLabel(), VoltageLabel()]
         
+        self.calibration = None
+        
         self.sample_rate_label1 = QLabel('Sample Rate:')
         self.sample_rate_combo = QComboBox()
         self.sample_rate_combo.addItem('976')
@@ -66,12 +149,18 @@ class IndustrialDualAnalogIn(PluginBase):
         self.sample_rate_label2 = QLabel('Samples per second')
         
         self.voltage_value = [None, None]
+        self.calibration_button = QPushButton('Show/Edit Calibration')
         
         self.sample_rate_combo.currentIndexChanged.connect(self.sample_rate_combo_index_changed)
+        self.calibration_button.clicked.connect(self.calibration_button_clicked)
         
         plot_list = [['Channel 0', Qt.red, self.get_voltage_value0],
                      ['Channel 1', Qt.blue, self.get_voltage_value1]]
         self.plot_widget = PlotWidget('Voltage [V]', plot_list)
+        
+        line = QFrame()
+        line.setFrameShape(QFrame.HLine)
+        line.setFrameShadow(QFrame.Sunken)
         
         layout_h = QHBoxLayout()
         layout_h.addStretch()
@@ -96,6 +185,8 @@ class IndustrialDualAnalogIn(PluginBase):
         layout.addLayout(layout_h1)
         layout.addWidget(self.plot_widget)
         layout.addLayout(layout_h2)
+        layout.addWidget(line)
+        layout.addWidget(self.calibration_button)
         
     def start(self):
         async_call(self.analog_in.get_voltage, 0, lambda x: self.cb_voltage(0, x), self.increase_error_count)
@@ -130,6 +221,13 @@ class IndustrialDualAnalogIn(PluginBase):
     
     def update_connected(self):
         pass
+    
+    def calibration_button_clicked(self):
+        if self.calibration is None:
+            self.calibration = Calibration(self)
+
+        self.calibration_button.setEnabled(False)
+        self.calibration.show()
     
     def sample_rate_combo_index_changed(self, index):
         async_call(self.analog_in.set_sample_rate, index, None, self.increase_error_count)
