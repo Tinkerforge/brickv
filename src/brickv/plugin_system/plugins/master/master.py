9b0e88084c59ceee2fc95fa7beb55ffd8501eba4
Matthias Bolte <matthias@tinkerforge.com>
2016-02-19 19:45:03 +0100

    Revert "Disable incomplete WIFI 2.0 support for 2.3.4 release"
    This reverts commit 2e4294c263d9cc7ac57d7cc0af6ef4023f968c32.

--- src/brickv/plugin_system/plugins/master/master.py
+++ src/brickv/plugin_system/plugins/master/master.py
@@ -100,8 +100,8 @@ class Master(PluginBase, Ui_Master):
         if self.firmware_version >= (2, 1, 0):
             async_call(self.master.is_ethernet_present, None, lambda p: is_present(p, self.master.EXTENSION_TYPE_ETHERNET, 'Ethernet Extension'), self.increase_error_count)
 
-        #if self.firmware_version >= (2, 4, 0):
-        #    async_call(self.master.is_wifi2_present, None, lambda p: is_present(p, self.master.EXTENSION_TYPE_WIFI2, 'WIFI Extension 2.0'), self.increase_error_count)
+        if self.firmware_version >= (2, 4, 0):
+            async_call(self.master.is_wifi2_present, None, lambda p: is_present(p, self.master.EXTENSION_TYPE_WIFI2, 'WIFI Extension 2.0'), self.increase_error_count)
 
         async_call(lambda: None, None, lambda: get_main_window().update_tree_view(), None)
 
@@ -163,7 +163,7 @@ class Master(PluginBase, Ui_Master):
             self.is_rs485_present_async(self.extension_type_preset[self.master.EXTENSION_TYPE_RS485])
             self.is_wifi_present_async(self.extension_type_preset[self.master.EXTENSION_TYPE_WIFI])
             self.is_ethernet_present_async(self.extension_type_preset[self.master.EXTENSION_TYPE_ETHERNET])
-            #self.is_wifi2_present_async(self.extension_type_preset[self.master.EXTENSION_TYPE_WIFI2])
+            self.is_wifi2_present_async(self.extension_type_preset[self.master.EXTENSION_TYPE_WIFI2])
 
         self.update_timer.start(1000)
 
