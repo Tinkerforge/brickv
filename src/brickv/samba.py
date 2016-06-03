f9e64b673febee8e90b7c2e6a2237611e3428cfe
Olaf Lüke <olaf@tinkerforge.com>
2016-06-03 10:19:32 +0200

    Add sam4e* defines

--- src/brickv/samba.py
+++ src/brickv/samba.py
@@ -102,6 +102,10 @@ CHIPID_CIDR_ATSAM4S2B_A = 0x289B07E0
 CHIPID_CIDR_ATSAM4S2B_B = 0x289B07E1
 CHIPID_CIDR_ATSAM4S4C_A = 0x28AB09E0
 CHIPID_CIDR_ATSAM4S4C_B = 0x28AB09E1
+CHIPID_CIDR_ATSAM4E8E_A = 0xA3CC0CE0
+CHIPID_CIDR_ATSAM4E8E_B = 0xA3CC0CE1
+CHIPID_CIDR_ATSAM4E16E_A = 0xA3CC0CE0
+CHIPID_CIDR_ATSAM4E16E_B = 0xA3CC0CE1
 
 EEFC_FMR = 0x400E0A00
 EEFC_FCR = 0x400E0A04
@@ -206,6 +210,20 @@ class SAMBA(object):
             self.flash_base = 0x400000
             self.flash_size = 0x40000
             self.flash_page_count = 512
+            self.flash_page_size = 512
+            self.flash_lockbit_count = 32
+        elif chipid in [CHIPID_CIDR_ATSAM4E8E_A, CHIPID_CIDR_ATSAM4E8E_B]:
+            self.sam_series = 4
+            self.flash_base = 0x400000
+            self.flash_size = 0x80000
+            self.flash_page_count = 1024
+            self.flash_page_size = 512
+            self.flash_lockbit_count = 32
+        elif chipid in [CHIPID_CIDR_ATSAM4E16E_A, CHIPID_CIDR_ATSAM4E16E_B]:
+            self.sam_series = 4
+            self.flash_base = 0x400000
+            self.flash_size = 0x80000
+            self.flash_page_count = 2048
             self.flash_page_size = 512
             self.flash_lockbit_count = 32
         else:
