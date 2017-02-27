2e0530cf8afb1544ce39f63c486a5a788aa5d656
Ishraq Ibne Ashraf <ishraq@tinkerforge.com>
2017-02-27 16:34:08 +0100

    bricklet-rs485: Add Modbus read discrete inputs function

--- src/brickv/bindings/bricklet_rs485.py
+++ src/brickv/bindings/bricklet_rs485.py
@@ -42,25 +42,28 @@ class BrickletRS485(Device):
     DEVICE_IDENTIFIER = 277
     DEVICE_DISPLAY_NAME = 'RS485 Bricklet'
 
-    CALLBACK_READ_CALLBACK = 38
-    CALLBACK_ERROR_COUNT = 39
-    CALLBACK_MODBUS_READ_COILS_REQUEST = 40
-    CALLBACK_MODBUS_READ_COILS_RESPONSE_LOW_LEVEL = 41
-    CALLBACK_MODBUS_READ_HOLDING_REGISTERS_REQUEST = 42
-    CALLBACK_MODBUS_READ_HOLDING_REGISTERS_RESPONSE_LOW_LEVEL = 43
-    CALLBACK_MODBUS_WRITE_SINGLE_COIL_REQUEST = 44
-    CALLBACK_MODBUS_WRITE_SINGLE_COIL_RESPONSE = 45
-    CALLBACK_MODBUS_WRITE_SINGLE_REGISTER_REQUEST = 46
-    CALLBACK_MODBUS_WRITE_SINGLE_REGISTER_RESPONSE = 47
-    CALLBACK_MODBUS_WRITE_MULTIPLE_COILS_REQUEST_LOW_LEVEL = 48
-    CALLBACK_MODBUS_WRITE_MULTIPLE_COILS_RESPONSE = 49
-    CALLBACK_MODBUS_WRITE_MULTIPLE_REGISTERS_REQUEST_LOW_LEVEL = 50
-    CALLBACK_MODBUS_WRITE_MULTIPLE_REGISTERS_RESPONSE = 51
+    CALLBACK_READ_CALLBACK = 40
+    CALLBACK_ERROR_COUNT = 41
+    CALLBACK_MODBUS_READ_COILS_REQUEST = 42
+    CALLBACK_MODBUS_READ_COILS_RESPONSE_LOW_LEVEL = 43
+    CALLBACK_MODBUS_READ_HOLDING_REGISTERS_REQUEST = 44
+    CALLBACK_MODBUS_READ_HOLDING_REGISTERS_RESPONSE_LOW_LEVEL = 45
+    CALLBACK_MODBUS_WRITE_SINGLE_COIL_REQUEST = 46
+    CALLBACK_MODBUS_WRITE_SINGLE_COIL_RESPONSE = 47
+    CALLBACK_MODBUS_WRITE_SINGLE_REGISTER_REQUEST = 48
+    CALLBACK_MODBUS_WRITE_SINGLE_REGISTER_RESPONSE = 49
+    CALLBACK_MODBUS_WRITE_MULTIPLE_COILS_REQUEST_LOW_LEVEL = 50
+    CALLBACK_MODBUS_WRITE_MULTIPLE_COILS_RESPONSE = 51
+    CALLBACK_MODBUS_WRITE_MULTIPLE_REGISTERS_REQUEST_LOW_LEVEL = 52
+    CALLBACK_MODBUS_WRITE_MULTIPLE_REGISTERS_RESPONSE = 53
+    CALLBACK_MODBUS_READ_DISCRETE_INPUTS_REQUEST = 54
+    CALLBACK_MODBUS_READ_DISCRETE_INPUTS_RESPONSE_LOW_LEVEL = 55
 
-    CALLBACK_MODBUS_READ_COILS_RESPONSE = -41
-    CALLBACK_MODBUS_READ_HOLDING_REGISTERS_RESPONSE = -43
-    CALLBACK_MODBUS_WRITE_MULTIPLE_COILS_REQUEST = -48
-    CALLBACK_MODBUS_WRITE_MULTIPLE_REGISTERS_REQUEST = -50
+    CALLBACK_MODBUS_READ_COILS_RESPONSE = -43
+    CALLBACK_MODBUS_READ_HOLDING_REGISTERS_RESPONSE = -45
+    CALLBACK_MODBUS_WRITE_MULTIPLE_COILS_REQUEST = -50
+    CALLBACK_MODBUS_WRITE_MULTIPLE_REGISTERS_REQUEST = -52
+    CALLBACK_MODBUS_READ_DISCRETE_INPUTS_RESPONSE = -55
 
     FUNCTION_WRITE = 1
     FUNCTION_READ = 2
@@ -99,6 +102,8 @@ class BrickletRS485(Device):
     FUNCTION_MODBUS_WRITE_MULTIPLE_COILS_LOW_LEVEL = 35
     FUNCTION_MODBUS_ANSWER_WRITE_MULTIPLE_REGISTERS_REQUEST = 36
     FUNCTION_MODBUS_WRITE_MULTIPLE_REGISTERS_LOW_LEVEL = 37
+    FUNCTION_MODBUS_ANSWER_READ_DISCRETE_INPUTS_REQUEST_LOW_LEVEL = 38
+    FUNCTION_MODBUS_READ_DISCRETE_INPUTS = 39
     FUNCTION_GET_SPITFP_ERROR_COUNT = 234
     FUNCTION_SET_BOOTLOADER_MODE = 235
     FUNCTION_GET_BOOTLOADER_MODE = 236
@@ -196,6 +201,8 @@ class BrickletRS485(Device):
         self.response_expected[BrickletRS485.FUNCTION_MODBUS_WRITE_MULTIPLE_COILS_LOW_LEVEL] = BrickletRS485.RESPONSE_EXPECTED_ALWAYS_TRUE
         self.response_expected[BrickletRS485.FUNCTION_MODBUS_ANSWER_WRITE_MULTIPLE_REGISTERS_REQUEST] = BrickletRS485.RESPONSE_EXPECTED_FALSE
         self.response_expected[BrickletRS485.FUNCTION_MODBUS_WRITE_MULTIPLE_REGISTERS_LOW_LEVEL] = BrickletRS485.RESPONSE_EXPECTED_ALWAYS_TRUE
+        self.response_expected[BrickletRS485.FUNCTION_MODBUS_ANSWER_READ_DISCRETE_INPUTS_REQUEST_LOW_LEVEL] = BrickletRS485.RESPONSE_EXPECTED_TRUE
+        self.response_expected[BrickletRS485.FUNCTION_MODBUS_READ_DISCRETE_INPUTS] = BrickletRS485.RESPONSE_EXPECTED_ALWAYS_TRUE
         self.response_expected[BrickletRS485.CALLBACK_READ_CALLBACK] = BrickletRS485.RESPONSE_EXPECTED_ALWAYS_FALSE
         self.response_expected[BrickletRS485.CALLBACK_ERROR_COUNT] = BrickletRS485.RESPONSE_EXPECTED_ALWAYS_FALSE
         self.response_expected[BrickletRS485.CALLBACK_MODBUS_READ_COILS_REQUEST] = BrickletRS485.RESPONSE_EXPECTED_ALWAYS_FALSE
@@ -210,6 +217,8 @@ class BrickletRS485(Device):
         self.response_expected[BrickletRS485.CALLBACK_MODBUS_WRITE_MULTIPLE_COILS_RESPONSE] = BrickletRS485.RESPONSE_EXPECTED_ALWAYS_FALSE
         self.response_expected[BrickletRS485.CALLBACK_MODBUS_WRITE_MULTIPLE_REGISTERS_REQUEST_LOW_LEVEL] = BrickletRS485.RESPONSE_EXPECTED_ALWAYS_FALSE
         self.response_expected[BrickletRS485.CALLBACK_MODBUS_WRITE_MULTIPLE_REGISTERS_RESPONSE] = BrickletRS485.RESPONSE_EXPECTED_ALWAYS_FALSE
+        self.response_expected[BrickletRS485.CALLBACK_MODBUS_READ_DISCRETE_INPUTS_REQUEST] = BrickletRS485.RESPONSE_EXPECTED_ALWAYS_FALSE
+        self.response_expected[BrickletRS485.CALLBACK_MODBUS_READ_DISCRETE_INPUTS_RESPONSE_LOW_LEVEL] = BrickletRS485.RESPONSE_EXPECTED_ALWAYS_FALSE
         self.response_expected[BrickletRS485.FUNCTION_GET_SPITFP_ERROR_COUNT] = BrickletRS485.RESPONSE_EXPECTED_ALWAYS_TRUE
         self.response_expected[BrickletRS485.FUNCTION_SET_BOOTLOADER_MODE] = BrickletRS485.RESPONSE_EXPECTED_ALWAYS_TRUE
         self.response_expected[BrickletRS485.FUNCTION_GET_BOOTLOADER_MODE] = BrickletRS485.RESPONSE_EXPECTED_ALWAYS_TRUE
@@ -237,11 +246,14 @@ class BrickletRS485(Device):
         self.callback_formats[BrickletRS485.CALLBACK_MODBUS_WRITE_MULTIPLE_COILS_RESPONSE] = 'B b H H'
         self.callback_formats[BrickletRS485.CALLBACK_MODBUS_WRITE_MULTIPLE_REGISTERS_REQUEST_LOW_LEVEL] = 'B H H H H 27H'
         self.callback_formats[BrickletRS485.CALLBACK_MODBUS_WRITE_MULTIPLE_REGISTERS_RESPONSE] = 'B b H H'
+        self.callback_formats[BrickletRS485.CALLBACK_MODBUS_READ_DISCRETE_INPUTS_REQUEST] = 'B H H'
+        self.callback_formats[BrickletRS485.CALLBACK_MODBUS_READ_DISCRETE_INPUTS_RESPONSE_LOW_LEVEL] = 'B b H H 58B'
 
         self.low_level_callbacks[BrickletRS485.CALLBACK_MODBUS_READ_COILS_RESPONSE_LOW_LEVEL] = [BrickletRS485.CALLBACK_MODBUS_READ_COILS_RESPONSE, {'stream': {'fixed_total_length': None}}, None]
         self.low_level_callbacks[BrickletRS485.CALLBACK_MODBUS_READ_HOLDING_REGISTERS_RESPONSE_LOW_LEVEL] = [BrickletRS485.CALLBACK_MODBUS_READ_HOLDING_REGISTERS_RESPONSE, {'stream': {'fixed_total_length': None}}, None]
         self.low_level_callbacks[BrickletRS485.CALLBACK_MODBUS_WRITE_MULTIPLE_COILS_REQUEST_LOW_LEVEL] = [BrickletRS485.CALLBACK_MODBUS_WRITE_MULTIPLE_COILS_REQUEST, {'stream': {'fixed_total_length': None}}, None]
         self.low_level_callbacks[BrickletRS485.CALLBACK_MODBUS_WRITE_MULTIPLE_REGISTERS_REQUEST_LOW_LEVEL] = [BrickletRS485.CALLBACK_MODBUS_WRITE_MULTIPLE_REGISTERS_REQUEST, {'stream': {'fixed_total_length': None}}, None]
+        self.low_level_callbacks[BrickletRS485.CALLBACK_MODBUS_READ_DISCRETE_INPUTS_RESPONSE_LOW_LEVEL] = [BrickletRS485.CALLBACK_MODBUS_READ_DISCRETE_INPUTS_RESPONSE, {'stream': {'fixed_total_length': None}}, None]
 
     def write(self, message, length):
         """
@@ -525,6 +537,18 @@ class BrickletRS485(Device):
         """
         return self.ipcon.send_request(self, BrickletRS485.FUNCTION_MODBUS_WRITE_MULTIPLE_REGISTERS_LOW_LEVEL, (slave_address, starting_address, count, stream_total_length, stream_chunk_offset, stream_chunk_data), 'B H H H H 27H', 'B')
 
+    def modbus_answer_read_discrete_inputs_request_low_level(self, request_id, stream_total_length, stream_chunk_offset, stream_chunk_data):
+        """
+        -
+        """
+        self.ipcon.send_request(self, BrickletRS485.FUNCTION_MODBUS_ANSWER_READ_DISCRETE_INPUTS_REQUEST_LOW_LEVEL, (request_id, stream_total_length, stream_chunk_offset, stream_chunk_data), 'B H H 59B', '')
+
+    def modbus_read_discrete_inputs(self, slave_address, starting_address, count):
+        """
+        -
+        """
+        return self.ipcon.send_request(self, BrickletRS485.FUNCTION_MODBUS_READ_DISCRETE_INPUTS, (slave_address, starting_address, count), 'B H H', 'B')
+
     def get_spitfp_error_count(self):
         """
         Returns the error count for the communication between Brick and Bricklet.
@@ -724,6 +748,24 @@ class BrickletRS485(Device):
             result = self.modbus_write_multiple_registers_low_level(slave_address, starting_address, count, stream_total_length, stream_chunk_offset, stream_chunk_data)
 
             stream_chunk_offset += 27
+
+        return result
+
+    def modbus_answer_read_discrete_inputs_request(self, request_id, data):
+        stream_total_length = len(data)
+        stream_chunk_offset = 0
+        result = None
+
+        while stream_chunk_offset < stream_total_length:
+            stream_chunk_data = data[stream_chunk_offset:stream_chunk_offset + 59]
+
+            if len(stream_chunk_data) < 59:
+                stream_chunk_data.extend([0]*(59 - len(stream_chunk_data)))
+
+            # FIXME: validate that the result of all the low-level calls is identical
+            result = self.modbus_answer_read_discrete_inputs_request_low_level(request_id, stream_total_length, stream_chunk_offset, stream_chunk_data)
+
+            stream_chunk_offset += 59
 
         return result
 
--- src/brickv/plugin_system/plugins/rs485/rs485.py
+++ src/brickv/plugin_system/plugins/rs485/rs485.py
@@ -45,6 +45,7 @@ MODBUS_F_IDX_WRITE_SINGLE_COIL = 2
 MODBUS_F_IDX_WRITE_SINGLE_REGISTER = 3
 MODBUS_F_IDX_WRITE_MULTIPLE_COILS = 4
 MODBUS_F_IDX_WRITE_MULTIPLE_REGISTERS = 5
+MODBUS_F_IDX_READ_DISCRETE_INPUTS = 6
 
 class RS485(COMCUPluginBase, Ui_RS485):
     qtcb_read = pyqtSignal(object, int)
@@ -63,6 +64,8 @@ class RS485(COMCUPluginBase, Ui_RS485):
     qtcb_modbus_write_multiple_coils_response = pyqtSignal(int, int, int, int)
     qtcb_modbus_write_multiple_registers_request = pyqtSignal(int, int, int, int, object)
     qtcb_modbus_write_multiple_registers_response = pyqtSignal(int, int, int, int)
+    qtcb_modbus_read_discrete_inputs_request = pyqtSignal(int, int, int)
+    qtcb_modbus_read_discrete_inputs_response = pyqtSignal(int, int, int, object)
 
     def __init__(self, *args):
         COMCUPluginBase.__init__(self, BrickletRS485, *args)
@@ -92,6 +95,8 @@ class RS485(COMCUPluginBase, Ui_RS485):
         self.qtcb_modbus_write_multiple_coils_response.connect(self.cb_modbus_write_multiple_coils_response)
         self.qtcb_modbus_write_multiple_registers_request.connect(self.cb_modbus_write_multiple_registers_request)
         self.qtcb_modbus_write_multiple_registers_response.connect(self.cb_modbus_write_multiple_registers_response)
+        self.qtcb_modbus_read_discrete_inputs_request.connect(self.cb_modbus_read_discrete_inputs_request)
+        self.qtcb_modbus_read_discrete_inputs_response.connect(self.cb_modbus_read_discrete_inputs_response)
 
         self.rs485.register_callback(self.rs485.CALLBACK_READ_CALLBACK,
                                      self.qtcb_read.emit)
@@ -132,6 +137,12 @@ class RS485(COMCUPluginBase, Ui_RS485):
 
         self.rs485.register_callback(self.rs485.CALLBACK_MODBUS_WRITE_MULTIPLE_REGISTERS_RESPONSE,
                                      self.qtcb_modbus_write_multiple_registers_response.emit)
+
+        self.rs485.register_callback(self.rs485.CALLBACK_MODBUS_READ_DISCRETE_INPUTS_REQUEST,
+                                     self.qtcb_modbus_read_discrete_inputs_request.emit)
+
+        self.rs485.register_callback(self.rs485.CALLBACK_MODBUS_READ_DISCRETE_INPUTS_RESPONSE,
+                                     self.qtcb_modbus_read_discrete_inputs_response.emit)
 
         self.rs485_input_combobox.addItem("")
         self.rs485_input_combobox.lineEdit().setMaxLength(58)
@@ -181,16 +192,16 @@ class RS485(COMCUPluginBase, Ui_RS485):
                                 self.rs485_input_line_ending_lineedit]
 
         self.gui_group_modbus_master = [self.modbus_master_function_label,
-                                            self.modbus_master_function_combobox,
-                                            self.modbus_master_slave_address_label,
-                                            self.modbus_master_slave_address_spinbox,
-                                            self.modbus_master_param1_label,
-                                            self.modbus_master_param1_spinbox,
-                                            self.modbus_master_param2_label,
-                                            self.modbus_master_param2_spinbox,
-                                            self.modbus_master_request_timeout_label,
-                                            self.modbus_master_request_timeout_spinbox,
-                                            self.modbus_master_send_button]
+                                        self.modbus_master_function_combobox,
+                                        self.modbus_master_slave_address_label,
+                                        self.modbus_master_slave_address_spinbox,
+                                        self.modbus_master_param1_label,
+                                        self.modbus_master_param1_spinbox,
+                                        self.modbus_master_param2_label,
+                                        self.modbus_master_param2_spinbox,
+                                        self.modbus_master_request_timeout_label,
+                                        self.modbus_master_request_timeout_spinbox,
+                                        self.modbus_master_send_button]
 
         self.gui_group_modbus_slave = [self.modbus_slave_address_label,
                                        self.modbus_slave_address_spinbox]
@@ -297,6 +308,16 @@ class RS485(COMCUPluginBase, Ui_RS485):
 
             self.modbus_master_send_button.setEnabled(False)
 
+        elif self.modbus_master_function_combobox.currentIndex() == MODBUS_F_IDX_READ_DISCRETE_INPUTS:
+            rid = self.rs485.modbus_read_discrete_inputs(self.modbus_master_slave_address_spinbox.value(),
+                                                         self.modbus_master_param1_spinbox.value(),
+                                                         self.modbus_master_param2_spinbox.value())
+            if rid == 0:
+                # Error.
+                return
+
+            self.modbus_master_send_button.setEnabled(False)
+
     def modbus_master_function_changed(self, function):
         if function == MODBUS_F_IDX_READ_COILS:
             self.modbus_master_param1_label.setText('Starting Address:')
@@ -346,6 +367,14 @@ class RS485(COMCUPluginBase, Ui_RS485):
             self.modbus_master_param2_label.setText('Number of Registers:')
             self.modbus_master_param2_spinbox.setMinimum(1)
             self.modbus_master_param2_spinbox.setMaximum(123)
+
+        elif function == MODBUS_F_IDX_READ_DISCRETE_INPUTS:
+            self.modbus_master_param1_label.setText('Starting Address:')
+            self.modbus_master_param1_spinbox.setMinimum(0)
+            self.modbus_master_param1_spinbox.setMaximum(65535)
+            self.modbus_master_param2_label.setText('Number of Coils:')
+            self.modbus_master_param2_spinbox.setMinimum(1)
+            self.modbus_master_param2_spinbox.setMaximum(2000)
 
     def mode_changed(self, mode):
         if mode == MODE_RS485:
@@ -668,6 +697,54 @@ class RS485(COMCUPluginBase, Ui_RS485):
             str(starting_address) + \
             ', COUNT=' + \
             str(count) + \
+            '\n\n'
+
+        self.text.moveCursor(QTextCursor.End)
+        self.text.insertPlainText(a)
+        self.text.moveCursor(QTextCursor.End)
+
+        self.modbus_master_send_button.setEnabled(True)
+
+    def cb_modbus_read_discrete_inputs_request(self, request_id, starting_address, count):
+        a = 'READ DISCRETE INPUTS REQUEST: ' + \
+            'REQUEST ID=' + \
+            str(request_id) + \
+            ', STARTING ADDRESS=' + \
+            str(starting_address) + \
+            ', COUNT=' + \
+            str(count) + \
+            '\n\n'
+
+        data = []
+
+        q, r = divmod(count, 8)
+
+        if r != 0:
+            q = q + 1
+
+        for i in range(q):
+            data.append(random.randint(1, 255))
+
+        self.rs485.modbus_answer_read_discrete_inputs_request(request_id, data)
+
+        self.text.moveCursor(QTextCursor.End)
+        self.text.insertPlainText(a)
+        self.text.moveCursor(QTextCursor.End)
+
+    def cb_modbus_read_discrete_inputs_response(self,
+                                                reconstruction_status,
+                                                request_id,
+                                                exception_code,
+                                                data):
+        a = 'READ DISCRETE INPUTS RESPONSE: ' + \
+            'EXCEPTION CODE=' + \
+            str(exception_code) + \
+            ', REQUEST ID=' + \
+            str(request_id) + \
+            ', RECONSTRUCTION STATUS=' + \
+            str(reconstruction_status) + \
+            ', DATA=' + \
+            str(data) + \
             '\n\n'
 
         self.text.moveCursor(QTextCursor.End)
