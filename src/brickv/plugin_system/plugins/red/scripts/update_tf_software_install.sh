59a78ddbb3d03dba297834fc572393f82c159cfa
Ishraq Ibne Ashraf <ishraq@tinkerforge.com>
2017-04-24 11:09:22 +0200

    Discard all install script output. This solves the problem of pipe being full and the update process failing

--- src/brickv/plugin_system/plugins/red/scripts/update_tf_software_install.sh
+++ src/brickv/plugin_system/plugins/red/scripts/update_tf_software_install.sh
@@ -8,7 +8,7 @@ echostderr() { echo "$@" 1>&2; }
 
 case "$1" in
   "brickv")
-    /usr/bin/dpkg -i $3;
+    /usr/bin/dpkg -i $3 &> /dev/null;
 
     if [ $? = 0 ]; then
       exit 0
@@ -19,88 +19,88 @@ case "$1" in
     ;;
 
   "c")
-    /usr/bin/unzip -q -d $2/bindings/c $3 && \
-    cd $2/bindings/c/source && \
-    /usr/bin/make && \
-    prefix=/usr /usr/bin/make install
+    /usr/bin/unzip -q -d $2/bindings/c $3 &> /dev/null && \
+    cd $2/bindings/c/source &> /dev/null && \
+    /usr/bin/make &> /dev/null && \
+    prefix=/usr /usr/bin/make install &> /dev/null
 
     ;;
 
   "csharp")
-    /usr/bin/unzip -q -d $2/bindings/csharp $3 && \
-    cd $2/bindings/csharp && \
-    /bin/cp Tinkerforge.dll /usr/lib
+    /usr/bin/unzip -q -d $2/bindings/csharp $3 &> /dev/null && \
+    cd $2/bindings/csharp &> /dev/null && \
+    /bin/cp Tinkerforge.dll /usr/lib &> /dev/null
 
     ;;
 
   "delphi")
-    /usr/bin/unzip -q -d $2/bindings/delphi $3 && \
-    cd $2/bindings/delphi/source && \
-    export FPCDIR=/usr/lib/fpc/default && \
-    /usr/bin/fpcmake && \
-    /usr/bin/make && \
-    /usr/bin/make install && \
-    /usr/bin/make clean
+    /usr/bin/unzip -q -d $2/bindings/delphi $3 &> /dev/null && \
+    cd $2/bindings/delphi/source &> /dev/null && \
+    export FPCDIR=/usr/lib/fpc/default &> /dev/null && \
+    /usr/bin/fpcmake &> /dev/null&& \
+    /usr/bin/make &> /dev/null && \
+    /usr/bin/make install &> /dev/null&& \
+    /usr/bin/make clean &> /dev/null
 
     ;;
 
   "java")
-    /usr/bin/unzip -q -d $2/bindings/java $3
+    /usr/bin/unzip -q -d $2/bindings/java $3 &> /dev/null
 
     ;;
 
   "javascript")
-    /usr/bin/unzip -q -d $2/bindings/javascript $3
+    /usr/bin/unzip -q -d $2/bindings/javascript $3 &> /dev/null
 
     ;;
 
   "matlab")
-    /usr/bin/unzip -q -d $2/bindings/matlab $3
+    /usr/bin/unzip -q -d $2/bindings/matlab $3 &> /dev/null
 
     ;;
 
   "perl")
-    /usr/bin/unzip -q -d $2/bindings/perl $3 && \
-    cd $2/bindings/perl/source && \
-    /usr/bin/perl Makefile.PL && \
-    /usr/bin/make all && \
-    /usr/bin/make test && \
-    /usr/bin/make install
+    /usr/bin/unzip -q -d $2/bindings/perl $3 &> /dev/null && \
+    cd $2/bindings/perl/source &> /dev/null && \
+    /usr/bin/perl Makefile.PL &> /dev/null && \
+    /usr/bin/make all &> /dev/null && \
+    /usr/bin/make test &> /dev/null && \
+    /usr/bin/make install &> /dev/null
 
     ;;
 
   "php")
-    /usr/bin/unzip -q -d $2/bindings/php $3 && \
-    cd $2/bindings/php && \
-    /usr/bin/pear install Tinkerforge.tgz
+    /usr/bin/unzip -q -d $2/bindings/php $3 &> /dev/null && \
+    cd $2/bindings/php &> /dev/null && \
+    /usr/bin/pear install Tinkerforge.tgz &> /dev/null
 
     ;;
 
   "python")
-    /usr/bin/unzip -q -d $2/bindings/python $3 && \
-    cd $2/bindings/python/source && \
-    /usr/bin/python2 setup.py install; \
-    /usr/bin/python3 setup.py install
+    /usr/bin/unzip -q -d $2/bindings/python $3 &> /dev/null && \
+    cd $2/bindings/python/source &> /dev/null && \
+    /usr/bin/python2 setup.py install &> /dev/null; \
+    /usr/bin/python3 setup.py install &> /dev/null
 
     ;;
 
   "ruby")
-    /usr/bin/unzip -q -d $2/bindings/ruby $3 && \
-    cd $2/bindings/ruby && \
-    /usr/bin/gem install tinkerforge.gem
+    /usr/bin/unzip -q -d $2/bindings/ruby $3 &> /dev/null && \
+    cd $2/bindings/ruby &> /dev/null && \
+    /usr/bin/gem install tinkerforge.gem &> /dev/null
 
     ;;
 
   "shell")
-    /usr/bin/unzip -q -d $2/bindings/shell $3 && \
-    cd $2/bindings/shell && \
-    /bin/cp tinkerforge /usr/local/bin && \
-    /bin/cp tinkerforge-bash-completion.sh /etc/bash_completion.d
+    /usr/bin/unzip -q -d $2/bindings/shell $3 &> /dev/null && \
+    cd $2/bindings/shell &> /dev/null && \
+    /bin/cp tinkerforge /usr/local/bin &> /dev/null && \
+    /bin/cp tinkerforge-bash-completion.sh /etc/bash_completion.d &> /dev/null
 
     ;;
 
   "vbnet")
-    /usr/bin/unzip -q -d $2/bindings/vbnet $3
+    /usr/bin/unzip -q -d $2/bindings/vbnet $3 &> /dev/null
 
     ;;
 esac
