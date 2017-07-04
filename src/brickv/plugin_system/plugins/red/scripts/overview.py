d8cf6544b998c06f9990fcd1b9ee480132191a04
Ishraq Ibne Ashraf <ishraq@tinkerforge.com>
2017-07-04 11:34:10 +0200

    red-brick: Make the overview tab compatible with the psutil version of Debian stretch

--- src/brickv/plugin_system/plugins/red/scripts/overview.py
+++ src/brickv/plugin_system/plugins/red/scripts/overview.py
@@ -32,7 +32,10 @@ du = psutil.disk_usage("/")
 all_process_info = []
 
 for p in psutil.process_iter(): # update CPU usage info
-    p.get_cpu_percent(interval=0)
+    if psutil.version_info >= (5, 0, 1):
+        p.cpu_percent(interval=0)
+    else:
+        p.get_cpu_percent(interval=0)
 
 if len(sys.argv) < 2:
     result['cpu_used'] = psutil.cpu_percent(1)
@@ -45,22 +48,41 @@ for p in psutil.process_iter():
     if p.pid == own_pid:
         continue
 
-    process_dict = {'cmd': ' '.join(get_cmdline(p)),
-                    'name': get_name(p),
-                    'pid': p.pid,
-                    'user': get_username(p),
-                    'cpu': int(p.get_cpu_percent(interval=0) * 10),
-                    'mem': int(p.get_memory_percent() * 10)}
+    if psutil.version_info >= (5, 0, 1):
+        process_dict = {'cmd': ' '.join(get_cmdline(p)),
+                        'name': get_name(p),
+                        'pid': p.pid,
+                        'user': get_username(p),
+                        'cpu': int(p.cpu_percent(interval=0) * 10),
+                        'mem': int(p.memory_percent() * 10)}
+    else:
+        process_dict = {'cmd': ' '.join(get_cmdline(p)),
+                        'name': get_name(p),
+                        'pid': p.pid,
+                        'user': get_username(p),
+                        'cpu': int(p.get_cpu_percent(interval=0) * 10),
+                        'mem': int(p.get_memory_percent() * 10)}
+
     all_process_info.append(process_dict)
 
 result['processes'] = all_process_info
 
-result['mem_used'] = psutil.used_phymem() - psutil.phymem_buffers() - psutil.cached_phymem()
-result['mem_total'] = psutil.TOTAL_PHYMEM
+if psutil.version_info >= (5, 0, 1):
+    mem_info = psutil.virtual_memory()
+    result['mem_used'] = mem_info.used
+    result['mem_total'] = mem_info.total
+else:
+    result['mem_used'] = psutil.used_phymem() - psutil.phymem_buffers() - psutil.cached_phymem()
+    result['mem_total'] = psutil.TOTAL_PHYMEM
+
 result['disk_used'] = du.used
 result['disk_total'] = du.total
 
-interfaces = psutil.network_io_counters(pernic=True)
+if psutil.version_info >= (5, 0, 1):
+    interfaces = psutil.net_io_counters(pernic=True)
+else:
+    interfaces = psutil.network_io_counters(pernic=True)
+
 if 'tunl0' in interfaces: # ignore tunl0 interface
     del interfaces['tunl0']
 
