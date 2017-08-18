37c2a1ba05e46fd846d06f18755a1b3210f46ffa
Ishraq Ibne Ashraf <ishraq@tinkerforge.com>
2017-08-18 12:29:18 +0200

    red-brick: NetworkManager integration: Disable hidden WiFi network connect button for test image release

--- src/brickv/plugin_system/plugins/red/red_tab_settings_network.py
+++ src/brickv/plugin_system/plugins/red/red_tab_settings_network.py
@@ -744,6 +744,10 @@ class REDTabSettingsNetwork(QtGui.QWidget, Ui_REDTabSettingsNetwork):
             self.chkbox_net_wireless_key_show.setChecked(False)
             self.chkbox_net_wireless_key_show.show()
             self.pbutton_net_wireless_connect_hidden.setEnabled(True)
+
+            # FIXME: This is only for the demo image, fix it for final release
+            if not self.image_version_lt_1_10:
+                self.pbutton_net_wireless_connect_hidden.setEnabled(False)
         else:
             self.label_ap.hide()
             self.tree_net_wireless_ap.hide()
