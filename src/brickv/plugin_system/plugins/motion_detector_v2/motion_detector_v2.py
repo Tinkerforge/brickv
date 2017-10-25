d2d778a7129995e9b8414d4c18f51ab8d880ad6a
Olaf Lüke <olaf@tinkerforge.com>
2017-10-25 17:39:01 +0200

    Motion Detector V2 will use all blue leds, change gui accordingly

--- src/brickv/plugin_system/plugins/motion_detector_v2/motion_detector_v2.py
+++ src/brickv/plugin_system/plugins/motion_detector_v2/motion_detector_v2.py
@@ -51,35 +51,30 @@ class MotionDetectorV2(COMCUPluginBase, Ui_MotionDetectorV2):
         self.motion_detector_v2.register_callback(self.motion_detector_v2.CALLBACK_DETECTION_CYCLE_ENDED,
                                                   self.qtcb_detection_cylce_ended.emit)
 
-        self.r_syncer = SliderSpinSyncer(self.slider_r, self.spin_r, self.rgb_changed, spin_signal='valueChanged')
-        self.g_syncer = SliderSpinSyncer(self.slider_g, self.spin_g, self.rgb_changed, spin_signal='valueChanged')
-        self.b_syncer = SliderSpinSyncer(self.slider_b, self.spin_b, self.rgb_changed, spin_signal='valueChanged')
+        self.left_syncer = SliderSpinSyncer(self.slider_left, self.spin_left, self.indicator_changed, spin_signal='valueChanged')
+        self.right_syncer = SliderSpinSyncer(self.slider_right, self.spin_right, self.indicator_changed, spin_signal='valueChanged')
+        self.bottom_syncer = SliderSpinSyncer(self.slider_bottom, self.spin_bottom, self.indicator_changed, spin_signal='valueChanged')
 
-        self.h_syncer = SliderSpinSyncer(self.slider_h, self.spin_h, self.hsl_changed, spin_signal='valueChanged')
-        self.s_syncer = SliderSpinSyncer(self.slider_s, self.spin_s, self.hsl_changed, spin_signal='valueChanged')
-        self.l_syncer = SliderSpinSyncer(self.slider_l, self.spin_l, self.hsl_changed, spin_signal='valueChanged')
+        self.all_syncer = SliderSpinSyncer(self.slider_all, self.spin_all, self.all_changed, spin_signal='valueChanged')
 
-        def set_color(r, g, b):
+        def set_indicator(l, r, b):
             self.changing = True
-            self.spin_r.setValue(r)
-            self.spin_g.setValue(g)
-            self.spin_b.setValue(b)
+            self.spin_left.setValue(l)
+            self.spin_right.setValue(r)
+            self.spin_bottom.setValue(b)
             self.changing = False
-            self.rgb_changed()
+            self.indicator_changed()
 
-        self.button_black.clicked.connect(lambda: set_color(0, 0, 0))
-        self.button_white.clicked.connect(lambda: set_color(255, 255, 255))
-        self.button_red.clicked.connect(lambda: set_color(255, 0, 0))
-        self.button_yellow.clicked.connect(lambda: set_color(255, 255, 0))
-        self.button_green.clicked.connect(lambda: set_color(0, 255, 0))
-        self.button_cyan.clicked.connect(lambda: set_color(0, 255, 255))
-        self.button_blue.clicked.connect(lambda: set_color(0, 0, 255))
-        self.button_magenta.clicked.connect(lambda: set_color(255, 0, 255))
+        self.button_off.clicked.connect(lambda: set_indicator(0, 0, 0))
+        self.button_on.clicked.connect(lambda: set_indicator(255, 255, 255))
+        self.button_left.clicked.connect(lambda: set_indicator(255, 0, 0))
+        self.button_right.clicked.connect(lambda: set_indicator(0, 255, 0))
+        self.button_bottom.clicked.connect(lambda: set_indicator(0, 0, 255))
         
         self.spinbox_sensitivity.valueChanged.connect(self.sensitivity_changed)
 
     def start(self):
-        async_call(self.motion_detector_v2.get_color, None, self.get_color_async, self.increase_error_count)
+        async_call(self.motion_detector_v2.get_indicator, None, self.get_indicator_async, self.increase_error_count)
         async_call(self.motion_detector_v2.get_motion_detected, None, self.get_motion_detected_async, self.increase_error_count)
         async_call(self.motion_detector_v2.get_sensitivity, None, self.get_sensitivity_async, self.increase_error_count)
 
@@ -104,48 +99,45 @@ class MotionDetectorV2(COMCUPluginBase, Ui_MotionDetectorV2):
     def cb_detection_cycle_ended(self):
         self.label_motion.setText("No Motion Detected")
 
-    def rgb_changed(self, *args):
+    def indicator_changed(self, *args):
         if self.changing:
             return
 
-        r, g, b = self.spin_r.value(), self.spin_g.value(), self.spin_b.value()
-        rgb = QColor(r, g, b)
-        h, s, l = rgb.hue(), rgb.saturation(), rgb.lightness()
+        left, right, bottom = self.spin_left.value(), self.spin_right.value(), self.spin_bottom.value()
 
         self.changing = True
-        self.spin_h.setValue(h)
-        self.spin_s.setValue(s)
-        self.spin_l.setValue(l)
+        self.spin_all.setValue((left+right+bottom)/3)
         self.changing = False
 
-        self.motion_detector_v2.set_color(r, g, b)
-        self.label_color.setStyleSheet('QLabel {{ background: #{:02x}{:02x}{:02x} }}'.format(r, g, b))
+        self.motion_detector_v2.set_indicator(left, right, bottom)
+        self.label_color_left.setStyleSheet('QLabel {{ background: #{:02x}{:02x}{:02x} }}'.format(0, 0, left))
+        self.label_color_right.setStyleSheet('QLabel {{ background: #{:02x}{:02x}{:02x} }}'.format(0, 0, right))
+        self.label_color_bottom.setStyleSheet('QLabel {{ background: #{:02x}{:02x}{:02x} }}'.format(0, 0, bottom))
 
-    def hsl_changed(self, *args):
+    def all_changed(self, *args):
         if self.changing:
             return
 
-        h, s, l = self.spin_h.value(), self.spin_s.value(), self.spin_l.value()
-        hsl = QColor()
-        hsl.setHsl(h, s, l)
-        r, g, b = hsl.red(), hsl.green(), hsl.blue()
+        x =  self.spin_all.value()
 
         self.changing = True
-        self.spin_r.setValue(r)
-        self.spin_g.setValue(g)
-        self.spin_b.setValue(b)
+        self.spin_left.setValue(x)
+        self.spin_right.setValue(x)
+        self.spin_bottom.setValue(x)
         self.changing = False
 
-        self.motion_detector_v2.set_color(r, g, b)
-        self.label_color.setStyleSheet('QLabel {{ background: #{:02x}{:02x}{:02x} }}'.format(r, g, b))
+        self.motion_detector_v2.set_indicator(x, x, x)
+        self.label_color_left.setStyleSheet('QLabel {{ background: #{:02x}{:02x}{:02x} }}'.format(0, 0, x))
+        self.label_color_right.setStyleSheet('QLabel {{ background: #{:02x}{:02x}{:02x} }}'.format(0, 0, x))
+        self.label_color_bottom.setStyleSheet('QLabel {{ background: #{:02x}{:02x}{:02x} }}'.format(0, 0, x))
 
-    def get_color_async(self, rgb):
+    def get_indicator_async(self, indicator):
         self.changing = True
-        self.spin_r.setValue(rgb.red)
-        self.spin_g.setValue(rgb.green)
-        self.spin_b.setValue(rgb.blue)
+        self.spin_left.setValue(indicator.top_left)
+        self.spin_right.setValue(indicator.top_right)
+        self.spin_bottom.setValue(indicator.bottom)
         self.changing = False
-        self.rgb_changed()
+        self.indicator_changed()
 
     def destroy(self):
         pass
