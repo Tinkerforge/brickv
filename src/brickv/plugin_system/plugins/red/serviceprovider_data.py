092acf5f4af0819685cb4039b5dd920727b24147
Matthias Bolte <matthias@tinkerforge.com>
2017-11-20 14:36:56 +0100

    red: Update provider database

--- src/brickv/plugin_system/plugins/red/serviceprovider_data.py
+++ src/brickv/plugin_system/plugins/red/serviceprovider_data.py
@@ -10143,7 +10143,7 @@ dict_country = \
  'co': 'Colombia',
  'cr': 'Costa Rica',
  'cy': 'Cyprus',
- 'cz': 'Czech Republic',
+ 'cz': 'Czechia',
  'de': 'Germany',
  'dk': 'Denmark',
  'do': 'Dominican Republic',
