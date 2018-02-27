ac1b0e707dce847d2ffdb611ec72e75a7e301717
Ishraq Ibne Ashraf <ishraq@tinkerforge.com>
2018-02-27 11:20:03 +0100

    red-brick: Fix update feature
    1. Python update issue
    2. Binary checks using "which"
    3. Check for last command exit code
    4. Remove before update when applicable

--- src/brickv/plugin_system/plugins/red/scripts/update_tf_software_install.sh
+++ src/brickv/plugin_system/plugins/red/scripts/update_tf_software_install.sh
@@ -8,7 +8,8 @@ echostderr() { echo "$@" 1>&2; }
 
 case "$1" in
   "brickv")
-    /usr/bin/dpkg -i $3 &> /dev/null;
+    $(/usr/bin/which apt-get) purge brickv -y &> /dev/null; \
+    $(/usr/bin/which dpkg) -i $3 &> /dev/null
 
     if [ $? = 0 ]; then
       exit 0
@@ -19,95 +20,186 @@ case "$1" in
     ;;
 
   "c")
-    /usr/bin/unzip -q -d $2/bindings/c $3 &> /dev/null && \
+    $(/usr/bin/which unzip) -q -d $2/bindings/c $3 &> /dev/null && \
     cd $2/bindings/c/source &> /dev/null && \
-    /usr/bin/make &> /dev/null && \
-    prefix=/usr /usr/bin/make install &> /dev/null
+    $(/usr/bin/which rm) -f /usr/lib/libtinkerforge.so &> /dev/null; \
+    $(/usr/bin/which make) &> /dev/null && \
+    $(/usr/bin/which make) prefix=/usr install &> /dev/null && \
+    $(/usr/bin/which make) clean &> /dev/null
+
+    if [ $? = 0 ]; then
+      exit 0
+    else
+      exit 1
+    fi
 
     ;;
 
   "csharp")
-    /usr/bin/unzip -q -d $2/bindings/csharp $3 &> /dev/null && \
+    $(/usr/bin/which unzip) -q -d $2/bindings/csharp $3 &> /dev/null && \
     cd $2/bindings/csharp &> /dev/null && \
-    /bin/cp Tinkerforge.dll /usr/lib &> /dev/null
+    $(/usr/bin/which rm) -f /usr/lib/Tinkerforge.dll &> /dev/null; \
+    $(/usr/bin/which cp) -f Tinkerforge.dll /usr/lib &> /dev/null
+
+    if [ $? = 0 ]; then
+      exit 0
+    else
+      exit 1
+    fi
 
     ;;
 
   "delphi")
-    /usr/bin/unzip -q -d $2/bindings/delphi $3 &> /dev/null && \
+    $(/usr/bin/which unzip) -q -d $2/bindings/delphi $3 &> /dev/null && \
     cd $2/bindings/delphi/source &> /dev/null && \
-    export FPCDIR=/usr/lib/fpc/default &> /dev/null && \
-    /usr/bin/fpcmake &> /dev/null&& \
-    /usr/bin/make &> /dev/null && \
-    /usr/bin/make install &> /dev/null&& \
-    /usr/bin/make clean &> /dev/null
+    $(/usr/bin/which rm) -rf /usr/lib/fpc/default/units/arm-linux/tinkerforge &> /dev/null; \
+    $(/usr/bin/which fpcmake) &> /dev/null && \
+    $(/usr/bin/which make) FPCDIR=/usr/lib/fpc/default &> /dev/null && \
+    $(/usr/bin/which make) FPCDIR=/usr/lib/fpc/default install &> /dev/null && \
+    $(/usr/bin/which make) clean &> /dev/null
+
+    if [ $? = 0 ]; then
+      exit 0
+    else
+      exit 1
+    fi
 
     ;;
 
   "java")
-    /usr/bin/unzip -q -d $2/bindings/java $3 &> /dev/null
+    $(/usr/bin/which unzip) -q -d $2/bindings/java $3 &> /dev/null
+
+    if [ $? = 0 ]; then
+      exit 0
+    else
+      exit 1
+    fi
 
     ;;
 
   "javascript")
-    /usr/bin/unzip -q -d $2/bindings/javascript $3 &> /dev/null && \
+    $(/usr/bin/which unzip) -q -d $2/bindings/javascript $3 &> /dev/null && \
     cd $2/bindings/javascript/nodejs &> /dev/null && \
     $(/usr/bin/which npm) uninstall -g tinkerforge &> /dev/null; \
-    $(/usr/bin/which npm) install -g tinkerforge.tgz &> /dev/null
+    $(/usr/bin/which npm) install -g ./tinkerforge.tgz &> /dev/null
+
+    if [ $? = 0 ]; then
+      exit 0
+    else
+      exit 1
+    fi
 
     ;;
 
   "matlab")
-    /usr/bin/unzip -q -d $2/bindings/matlab $3 &> /dev/null
+    $(/usr/bin/which unzip) -q -d $2/bindings/matlab $3 &> /dev/null
+
+    if [ $? = 0 ]; then
+      exit 0
+    else
+      exit 1
+    fi
 
     ;;
 
   "perl")
-    /usr/bin/unzip -q -d $2/bindings/perl $3 &> /dev/null && \
+    $(/usr/bin/which unzip) -q -d $2/bindings/perl $3 &> /dev/null && \
     cd $2/bindings/perl/source &> /dev/null && \
-    /usr/bin/perl Makefile.PL &> /dev/null && \
-    /usr/bin/make all &> /dev/null && \
-    /usr/bin/make test &> /dev/null && \
-    /usr/bin/make install &> /dev/null
+    $(/usr/bin/which cpanm) -f --uninstall Tinkerforge &> /dev/null; \
+    $(/usr/bin/which perl) Makefile.PL &> /dev/null && \
+    $(/usr/bin/which make) all &> /dev/null && \
+    $(/usr/bin/which make) install &> /dev/null
+
+    if [ $? = 0 ]; then
+      exit 0
+    else
+      exit 1
+    fi
 
     ;;
 
   "php")
-    /usr/bin/unzip -q -d $2/bindings/php $3 &> /dev/null && \
+    $(/usr/bin/which unzip) -q -d $2/bindings/php $3 &> /dev/null && \
     cd $2/bindings/php &> /dev/null && \
-    /usr/bin/pear install Tinkerforge.tgz &> /dev/null
+    $(/usr/bin/which pear) uninstall __uri/Tinkerforge &> /dev/null; \
+    $(/usr/bin/which pear) install ./Tinkerforge.tgz &> /dev/null
+
+    if [ $? = 0 ]; then
+      exit 0
+    else
+      exit 1
+    fi
 
     ;;
 
   "python")
-    /usr/bin/unzip -q -d $2/bindings/python $3 &> /dev/null && \
+    # Some python bindings release use "setuptools"(older) and some
+    # "distutils"(newer) to install the bindings. If the currently installed
+    # Python bindings were installed with "setuptools" then it is installed as
+    # ".egg" file.
+    #
+    # After an update if the newer bindings were installed with "distutils" then
+    # it will be installed as an ".egg-info".
+    #
+    # The ".egg" file seem to have priority while resolving for Python modules.
+    # As a result in this case the older modules will be loaded even after the
+    # update.
+    #
+    # To fix this problem first "pip uninstall" is used before updating the
+    # bindings.
+    $(/usr/bin/which unzip) -q -d $2/bindings/python $3 &> /dev/null && \
     cd $2/bindings/python/source &> /dev/null && \
-    /usr/bin/pip uninstall tinkerforge -y &> /dev/null; \
-    /usr/bin/pip3 uninstall tinkerforge -y &> /dev/null; \
-    /usr/bin/python2 setup.py install &> /dev/null; \
-    /usr/bin/python3 setup.py install &> /dev/null; \
-    /usr/bin/pip install tinkerforge &> /dev/null; \
-    /usr/bin/pip3 install tinkerforge &> /dev/null;
+    $(/usr/bin/which pip) uninstall tinkerforge -y &> /dev/null; \
+    $(/usr/bin/which pip3) uninstall tinkerforge -y &> /dev/null; \
+    $(/usr/bin/which python2) setup.py install &> /dev/null && \
+    $(/usr/bin/which python3) setup.py install &> /dev/null
+
+    if [ $? = 0 ]; then
+      exit 0
+    else
+      exit 1
+    fi
 
     ;;
 
   "ruby")
-    /usr/bin/unzip -q -d $2/bindings/ruby $3 &> /dev/null && \
+    $(/usr/bin/which unzip) -q -d $2/bindings/ruby $3 &> /dev/null && \
     cd $2/bindings/ruby &> /dev/null && \
-    /usr/bin/gem install tinkerforge.gem &> /dev/null
+    $(/usr/bin/which gem) uninstall tinkerforge &> /dev/null; \
+    $(/usr/bin/which gem) install --local --no-document ./tinkerforge.gem &> /dev/null
+
+    if [ $? = 0 ]; then
+      exit 0
+    else
+      exit 1
+    fi
 
     ;;
 
   "shell")
-    /usr/bin/unzip -q -d $2/bindings/shell $3 &> /dev/null && \
+    $(/usr/bin/which unzip) -q -d $2/bindings/shell $3 &> /dev/null && \
     cd $2/bindings/shell &> /dev/null && \
-    /bin/cp tinkerforge /usr/local/bin &> /dev/null && \
-    /bin/cp tinkerforge-bash-completion.sh /etc/bash_completion.d &> /dev/null
+    $(/usr/bin/which rm) -f /usr/local/bin/tinkerforge &> /dev/null; \
+    $(/usr/bin/which rm) -f /etc/bash_completion.d/tinkerforge-bash-completion.sh &> /dev/null; \
+    $(/usr/bin/which cp) -f tinkerforge /usr/local/bin &> /dev/null && \
+    $(/usr/bin/which cp) -f tinkerforge-bash-completion.sh /etc/bash_completion.d &> /dev/null
+
+    if [ $? = 0 ]; then
+      exit 0
+    else
+      exit 1
+    fi
 
     ;;
 
   "vbnet")
-    /usr/bin/unzip -q -d $2/bindings/vbnet $3 &> /dev/null
+    $(/usr/bin/which unzip) -q -d $2/bindings/vbnet $3 &> /dev/null
+
+    if [ $? = 0 ]; then
+      exit 0
+    else
+      exit 1
+    fi
 
     ;;
 esac
