6213b959ff619f6dfd8908bc59cca3b022d87e0e
Olaf Lüke <olaf@tinkerforge.com>
2018-08-20 18:06:06 +0200

    Add HAT Bricklet to __init_.py

--- src/brickv/plugin_system/plugins/__init__.py
+++ src/brickv/plugin_system/plugins/__init__.py
@@ -30,6 +30,7 @@ from brickv.plugin_system.plugins.gas_detector import device_class as gas_detect
 from brickv.plugin_system.plugins.gps import device_class as gps
 from brickv.plugin_system.plugins.gps_v2 import device_class as gps_v2
 from brickv.plugin_system.plugins.hall_effect import device_class as hall_effect
+from brickv.plugin_system.plugins.hat import device_class as hat
 from brickv.plugin_system.plugins.heart_rate import device_class as heart_rate
 from brickv.plugin_system.plugins.humidity import device_class as humidity
 from brickv.plugin_system.plugins.humidity_v2 import device_class as humidity_v2
@@ -154,6 +155,7 @@ device_classes = [
     gps,
     gps_v2,
     hall_effect,
+    hat,
     heart_rate,
     humidity,
     humidity_v2,
