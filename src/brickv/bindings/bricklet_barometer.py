82a2ad82e504564c1f37e997fb04546031dc86b6
Matthias Bolte <matthias@tinkerforge.com>
2018-10-05 15:26:09 +0200

    Update bindings

--- src/brickv/bindings/bricklet_barometer.py
+++ src/brickv/bindings/bricklet_barometer.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
 #############################################################
-# This file was automatically generated on 2018-09-28.      #
+# This file was automatically generated on 2018-10-05.      #
 #                                                           #
-# Python Bindings Version 2.1.18                            #
+# Python Bindings Version 2.1.19                            #
 #                                                           #
 # If you have a bugfix for this file and want to commit it, #
 # please fix the bug in the generator. You can find a link  #
