d1379c9518da2456ac263e1c9bb81ffc1f79c136
Matthias Bolte <matthias@tinkerforge.com>
2019-01-30 15:05:11 +0100

    red: Increase supported image version to 1.13

--- src/brickv/plugin_system/plugins/red/red.py
+++ src/brickv/plugin_system/plugins/red/red.py
@@ -41,7 +41,7 @@ from brickv.plugin_system.plugins.red.script_manager import ScriptManager
 from brickv.async_call import async_call
 from brickv.plugin_system.plugins.red.ui_red_update_tinkerforge_software import Ui_REDUpdateTinkerforgeSoftware
 
-MAX_IMAGE_VERSION = (1, 12)
+MAX_IMAGE_VERSION = (1, 13)
 
 class ImageVersion(object):
     string = None
