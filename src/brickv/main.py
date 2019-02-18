1d08ebbf27b7107af72cc1d3fa2779b4238a79b9
Erik Fleckstein <erik@tinkerforge.com>
2019-02-18 13:16:54 +0100

    Show uncaught exceptions on stderr and in QErrorMessage.

--- src/brickv/main.py
+++ src/brickv/main.py
@@ -58,13 +58,16 @@ def prepare_package(package_name):
 prepare_package('brickv')
 
 from PyQt5.QtGui import QIcon, QFont
-from PyQt5.QtWidgets import QApplication
-from PyQt5.QtCore import QEvent, pyqtSignal
+from PyQt5.QtWidgets import QApplication, QErrorMessage
+from PyQt5.QtCore import QEvent, pyqtSignal, qCritical
 
 from brickv import config
 from brickv.mainwindow import MainWindow
 from brickv.async_call import ASYNC_EVENT, async_event_handler
 from brickv.load_pixmap import load_pixmap
+
+from brickv.bindings.ip_connection import Error
+import traceback
 
 logging.basicConfig(level=config.LOGGING_LEVEL,
                     format=config.LOGGING_FORMAT,
@@ -76,7 +79,7 @@ class BrickViewer(QApplication):
 
     def __init__(self, *args, **kwargs):
         QApplication.__init__(self, *args, **kwargs)
-
+        QErrorMessage.qtHandler()
         self.object_creator_signal.connect(self.object_creator_slot)
         self.setWindowIcon(QIcon(load_pixmap('brickv-icon.png')))
 
@@ -89,7 +92,12 @@ class BrickViewer(QApplication):
 
         return QApplication.notify(self, receiver, event)
 
+def exception_hook(exctype, value, tb):
+    traceback.print_exception(etype=exctype, value=value, tb=tb)
+    qCritical("Please report this error to info@tinkerforge.com:\n\nException type: {}\nException value:{}\nTraceback:{}".format(str(exctype), str(value), "".join(traceback.format_exception(etype=exctype, value=value, tb=tb))))
+
 def main():
+    sys.excepthook = exception_hook
     try:
         locale.setlocale(locale.LC_ALL, '')
     except locale.Error:
