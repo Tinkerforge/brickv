f08f1dea5f82eb9acb8ecbc86f9c68724b0174f3
Erik Fleckstein <erik@tinkerforge.com>
2019-02-27 13:58:10 +0100

    Sort Bricklets connected to Master before other Bricks.

--- src/brickv/mainwindow.py
+++ src/brickv/mainwindow.py
@@ -55,14 +55,27 @@ USER_ROLE_POSITION = Qt.UserRole + 1
 class DevicesProxyModel(QSortFilterProxyModel):
     # overrides QSortFilterProxyModel.lessThan
     def lessThan(self, left, right):
-        if left.column() == 2: # position
-            left_position = left.data(USER_ROLE_POSITION)
-            right_position = right.data(USER_ROLE_POSITION)
+        if left.column() != 2: # position
+            return QSortFilterProxyModel.lessThan(self, left, right)
 
-            if left_position != None and right_position != None:
-                return left_position < right_position
+        # Keep grouping intact.
+        left_position = left.data(USER_ROLE_POSITION)
+        right_position = right.data(USER_ROLE_POSITION)
 
-        return QSortFilterProxyModel.lessThan(self, left, right)
+        if left_position != None and right_position != None:
+            return left_position < right_position
+
+        # Sort letters before digits, so that Bricklets connected to a
+        # Master Brick are shown before Bricks stacked on the Master.
+        # Also sort extensions after letters (i.e Bricks), so they are shown last.
+        def get_sort_key(data):
+            diff = ord('z') - ord('0') + 1 # Put digits after letters
+            if 'Ext' in data:
+                diff += 10 # Put digits of extensions after normal digits
+                data = data[3:]
+            return ord(data) + (diff if data.isdigit() else 0)
+
+        return get_sort_key(left.data()) < get_sort_key(right.data())
 
 class MainWindow(QMainWindow, Ui_MainWindow):
     qtcb_enumerate = pyqtSignal(str, str, str, type((0,)), type((0,)), int, int)
