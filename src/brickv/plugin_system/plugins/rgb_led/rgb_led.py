e52b9273e1c8b89b235540ba61f742777024fa9b
Erik Fleckstein <erik@tinkerforge.com>
2019-03-12 13:19:05 +0100

    Reduce RGB LED color icon to 16x16px. (2/2)

--- src/brickv/plugin_system/plugins/rgb_led/rgb_led.py
+++ src/brickv/plugin_system/plugins/rgb_led/rgb_led.py
@@ -63,8 +63,8 @@ class RGBLED(PluginBase, Ui_RGBLED):
                                 [self.button_black, self.button_white, self.button_red, self.button_yellow,
                                  self.button_green, self.button_cyan, self.button_blue, self.button_magenta]):
             button.clicked.connect(lambda clicked, c = color: set_color(*c))
-            pixmap = QPixmap(32, 32)
-            QPainter(pixmap).fillRect(0, 0, 32, 32, QColor(*color))
+            pixmap = QPixmap(16, 16)
+            QPainter(pixmap).fillRect(0, 0, 16, 16, QColor(*color))
             button.setIcon(QIcon(pixmap))
             button.setToolButtonStyle(Qt.ToolButtonTextBesideIcon)
 
