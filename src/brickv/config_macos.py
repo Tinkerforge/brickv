aa67547bc8a3bfa8d743b2f7e844270a898ee543
Erik Fleckstein <erik@tinkerforge.com>
2019-03-13 11:06:05 +0100

    Make update search names consistent (2/2).

--- src/brickv/config_macos.py
+++ src/brickv/config_macos.py
@@ -97,10 +97,10 @@ def set_use_fusion_gui_style(value):
     set_plist_value('UseFusionGUIStyle', str(bool(value)).lower())
 
 def get_auto_search_for_updates():
-    return get_plist_value('SearchForUpdates', str(DEFAULT_AUTO_SEARCH_FOR_UPDATES).lower()) == 'true'
+    return get_plist_value('AutoSearchForUpdates', str(DEFAULT_AUTO_SEARCH_FOR_UPDATES).lower()) == 'true'
 
 def set_auto_search_for_updates(value):
-    set_plist_value('SearchForUpdates', str(bool(value)).lower())
+    set_plist_value('AutoSearchForUpdates', str(bool(value)).lower())
 
 def legacy_get_host():
     return get_plist_value('Host', DEFAULT_HOST)
