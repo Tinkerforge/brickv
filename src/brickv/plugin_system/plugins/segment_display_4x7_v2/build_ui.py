70d96001874e7515066aa979b488f25954238684
Erik Fleckstein <erik@tinkerforge.com>
2019-03-13 14:07:35 +0100

    Remove redundant build_ui script.

--- src/brickv/plugin_system/plugins/segment_display_4x7_v2/build_ui.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-
-import os
-
-def system(command):
-    if os.system(command) != 0:
-        exit(1)
-
-system("pyuic4 -o ui_segment_display_4x7_v2.py ui/segment_display_4x7_v2.ui")
