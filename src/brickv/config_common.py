f0dfacf62c260022affaf4255074495341488ba3
Matthias Bolte <matthias@tinkerforge.com>
2019-03-18 17:54:37 +0100

    Add missing logging import

--- src/brickv/config_common.py
+++ src/brickv/config_common.py
@@ -21,8 +21,8 @@ Free Software Foundation, Inc., 59 Temple Place - Suite 330,
 Boston, MA 02111-1307, USA.
 """
 
-import logging
 import sys
+import logging
 
 LOGGING_LEVEL = logging.ERROR
 LOGGING_FORMAT = '%(asctime)s <%(levelname)s> <%(filename)s:%(lineno)s> %(message)s'
--- src/brickv/config_windows.py
+++ src/brickv/config_windows.py
@@ -21,6 +21,7 @@ Free Software Foundation, Inc., 59 Temple Place - Suite 330,
 Boston, MA 02111-1307, USA.
 """
 
+import logging
 import winreg
 
 from brickv.config_common import *
