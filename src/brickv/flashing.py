08c4368526e67d2f7d2e76315e1676e606b30c24
Matthias Bolte <matthias@tinkerforge.com>
2019-04-29 16:51:11 +0200

    flashing: Don't rely on GC alone to destroy/cancel progress dialog

--- src/brickv/flashing.py
+++ src/brickv/flashing.py
@@ -544,6 +544,8 @@ class FlashingWindow(QDialog, Ui_Flashing):
 
             self.update_ui_state()
 
+        progress.cancel()
+
         # For some reason the progress dialog is not hidden if the parent is not removed.
         # Other approaches such as hide, close, cancel, reset, QApplication.processEvents,
         # a single shot timer and setting progress to None don't work everytime.
