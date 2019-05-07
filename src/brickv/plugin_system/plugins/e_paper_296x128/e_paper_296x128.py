4c6b74cb647ca0ffdc80b5c5e89f1fe3ef0acf52
Olaf Lüke <olaf@tinkerforge.com>
2019-05-07 17:43:09 +0200

    fix E-Paper constant naming

--- src/brickv/plugin_system/plugins/e_paper_296x128/e_paper_296x128.py
+++ src/brickv/plugin_system/plugins/e_paper_296x128/e_paper_296x128.py
@@ -55,7 +55,7 @@ class EPaper296x128(COMCUPluginBase, Ui_EPaper296x128):
         self.color_radio_white.toggled.connect(self.radio_toggled)
         self.color_radio_red.toggled.connect(self.radio_toggled)
 
-        self.display_type = self.epaper.DISPLAY_BLACK_WHITE_RED
+        self.display_type = self.epaper.DISPLAY_TYPE_BLACK_WHITE_RED
 
     def radio_toggled(self):
         if self.color_radio_black.isChecked():
@@ -63,7 +63,7 @@ class EPaper296x128(COMCUPluginBase, Ui_EPaper296x128):
         elif self.color_radio_white.isChecked():
             self.scribble_widget.set_foreground_color(QColor(Qt.white))
         elif self.color_radio_red.isChecked():
-            if self.display_type == self.epaper.DISPLAY_BLACK_WHITE_RED:
+            if self.display_type == self.epaper.DISPLAY_TYPE_BLACK_WHITE_RED:
                 self.scribble_widget.set_foreground_color(QColor(Qt.red))
             else:
                 self.scribble_widget.set_foreground_color(QColor(Qt.darkGray))
@@ -114,7 +114,7 @@ class EPaper296x128(COMCUPluginBase, Ui_EPaper296x128):
             for i in range(HEIGHT):
                 for j in range(WIDTH):
                     if pixels[i * WIDTH + j]:
-                        if self.display_type == self.epaper.DISPLAY_BLACK_WHITE_RED:
+                        if self.display_type == self.epaper.DISPLAY_TYPE_BLACK_WHITE_RED:
                             self.scribble_widget.image().setPixel(j, i, 0xFF0000)
                         else:
                             self.scribble_widget.image().setPixel(j, i, 0x808080)
@@ -124,7 +124,7 @@ class EPaper296x128(COMCUPluginBase, Ui_EPaper296x128):
     def get_display_type_async(self, display_type):
         self.display_type = display_type
 
-        if display_type == self.epaper.DISPLAY_BLACK_WHITE_RED:
+        if display_type == self.epaper.DISPLAY_TYPE_BLACK_WHITE_RED:
             color = 'Red'
         else:
             color = 'Grey'
