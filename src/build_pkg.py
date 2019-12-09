87df1ab2919b8357c004f2f4f574146446468c73
Matthias Bolte <matthias@tinkerforge.com>
2019-12-09 14:16:59 +0100

    Update changelogs

--- src/build_pkg.py
+++ src/build_pkg.py
@@ -170,7 +170,7 @@ def build_linux_flash_pkg():
 
     utils.build_debian_pkg()
 
-BRICK_LOGGER_VERSION = '2.1.1'
+BRICK_LOGGER_VERSION = '2.1.2'
 
 def build_logger_zip():
     print('building brick-logger ZIP file')
