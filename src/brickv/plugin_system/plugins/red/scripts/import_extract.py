cf7758ca4303763420fb5672599d932bf43ef5ba
Erik Fleckstein <erik@tinkerforge.com>
2022-10-25 09:54:37 +0200

    red: Fix import_extract.py Python2 compatibility.

--- src/brickv/plugin_system/plugins/red/scripts/import_extract.py
+++ src/brickv/plugin_system/plugins/red/scripts/import_extract.py
@@ -99,13 +99,13 @@ try:
 
             return prefix == abs_directory
 
-        def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+        def safe_extract(tar, path=".", members=None):
             for member in tar.getmembers():
                 member_path = os.path.join(path, member.name)
                 if not is_within_directory(path, member_path):
                     raise Exception("Attempted Path Traversal in Tar File")
 
-            tar.extractall(path, members, numeric_owner=numeric_owner)
+            tar.extractall(path, members)
 
         safe_extract(a, path=directory, members=members)
 
