51792c63b5bc12d726c1becf1a413a395178b8a4
Matthias Bolte <matthias@tinkerforge.com>
2023-01-30 12:31:38 +0100

    Update changelog and version

--- src/brickv/config_common.py
+++ src/brickv/config_common.py
@@ -30,7 +30,7 @@ LOGGING_LEVEL = logging.ERROR
 LOGGING_FORMAT = '%(asctime)s <%(levelname)s> <%(filename)s:%(lineno)s> %(message)s'
 LOGGING_DATEFMT = '%Y-%m-%d %H:%M:%S'
 
-BRICKV_VERSION = '2.4.23'
+BRICKV_VERSION = '2.4.24'
 BRICKV_TITLE = 'Brick Viewer ' + BRICKV_VERSION
 
 HOST_INFO_COUNT = 10
